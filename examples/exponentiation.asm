29cd5c0f9125473787d3dc448c6a106c4c60c54e
Christian Krause <christian.krause01@sap.com>
2018-01-21 21:15:26 +0100

    more exp stuff

--- examples/exponentiation.asm
+++ examples/exponentiation.asm
@@ -7,24 +7,26 @@
 ; output:
 ;   $2 = exp(m,n) = m^n
 
-mov $2,0
+mov $2,1           ; e = 1
+mov $3,0           ; f = 0
+mov $4,$1          ; i = n
+mov $5,$0          ; j = m
 
-mov $3,$1
-mov $4,$0
-
-lpb $3,2
+lpb $4,2
   dbg
-  add $2,$0
 
-  mov $5,1
-  sub $5,$4
-  lpb $5,1
-    sub $3,1
-    mov $4,$0
-    add $4,1
-    mov $5,0
-  lpe
+  add $3,$2        ; f = f + e
 
-  sub $4,1
+  sub $5,1         ; m = m - 1
+
+  mov $6,1         ; x = 1
+  sub $6,$5        ; x = x - m
+  lpb $6,1         ; if j = 0
+    mov $2,$3      ;   e = f
+    mov $3,0       ;   f = 0
+    mov $5,$0      ;   j = m
+    mov $6,0
+    sub $4,1       ;   i = i - 1
+  lpe              ; end if
 
 lpe
