5230cb06aeee4735c3bd5dd9beb044d7b2661625
Christian Krause <christian.krause01@sap.com>
2018-01-21 13:43:00 +0100

    more exp

--- examples/exponentiation.asm
+++ examples/exponentiation.asm
@@ -9,7 +9,22 @@
 
 mov $2,0
 
-lpb $1,1
+mov $3,$1
+mov $4,$0
+
+lpb $3,2
+  dbg
   add $2,$0
-  sub $1,1
+
+  mov $5,1
+  sub $5,$4
+  lpb $5,1
+    sub $3,1
+    mov $4,$0
+    add $4,1
+    mov $5,0
+  lpe
+
+  sub $4,1
+
 lpe
