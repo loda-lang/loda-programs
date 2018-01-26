f31705c885e538cfffe4643f0acdfcb5951a611b
Christian Krause <christian.krause01@sap.com>
2018-01-26 22:46:43 +0100

    fix exponentiation

--- examples/exponentiation.asm
+++ examples/exponentiation.asm
@@ -13,20 +13,22 @@ mov $4,$1          ; i = n
 mov $5,$0          ; j = m
 
 lpb $4,2
-  dbg
 
-  add $3,$2        ; f = f + e
-
-  sub $5,1         ; m = m - 1
+  mov $6,$5        ; x = j
+  lpb $6,1         ; if j > 0
+    sub $6,1       ; x = x - j
+    add $3,$2      ; f = f + e
+    sub $5,1         ; j = j - 1
+  lpe
 
   mov $6,1         ; x = 1
-  sub $6,$5        ; x = x - m
+  sub $6,$5        ; x = x - j
   lpb $6,1         ; if j = 0
     mov $2,$3      ;   e = f
     mov $3,0       ;   f = 0
     mov $5,$0      ;   j = m
-    mov $6,0
     sub $4,1       ;   i = i - 1
+    mov $6,0
   lpe              ; end if
 
 lpe
