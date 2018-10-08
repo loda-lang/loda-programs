8d4f7a27e02427f0184cb6f7190b1e3b618e247f
Christian Krause <me@ckrause.org>
2018-10-08 14:52:12 -0700

    program minimizer

--- programs/oeis/A178405.asm
+++ programs/oeis/A178405.asm
@@ -1,35 +1,43 @@
 ; A178405: Numbers such that the rounded up arithmetic mean of their digits differs from their digital root.
 ; 11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76
 
+mov $3,4
+mov $$6,$0
+mov $$1,$$4
+lpb $$1,7
+  mov $2,$$0
+  lpb $2,$7
+    sub $0,$3
+  lpe
+lpe
+add $$5,4
+mov $4,$$6
+lpb $3,$$5
+  mov $3,$5
+lpe
+mov $1,$0
+mov $0,1
+lpb $$3,$$0
+  mov $0,$4
+lpe
+mov $5,$$0
+mov $3,$$5
+add $1,7
+sub $1,$7
+sub $$7,$5
+add $6,$$0
+mov $7,$$3
+mov $3,$$3
+mov $$0,$1
 add $$6,6
-lpb $$1,$$5
-  add $$6,$$4
-lpe
-add $$7,$1
-add $5,$$6
-mov $4,$$5
-add $$4,4
-add $1,$5
-mov $7,$$6
-sub $2,$6
-sub $4,$$7
-sub $$2,$7
-sub $$6,$$1
-mov $$0,$6
-sub $$3,$6
-lpb $$0,$$6
-  add $0,$0
-  sub $6,3
-  add $6,7
-  mov $3,3
-  add $1,4
-lpe
-sub $$4,5
-mov $6,$$1
-add $$7,$5
-add $1,5
-lpb $$0,1
-  sub $$6,$1
-lpe
-sub $2,2
-mov $$1,5
+add $$5,$$2
+mov $$1,$4
+add $5,7
+sub $5,7
+sub $0,$4
+sub $$1,1
+sub $$1,$6
+sub $0,5
+mov $$2,$3
+sub $4,6
+mov $4,5
--- programs/oeis/A317945.asm
+++ programs/oeis/A317945.asm
@@ -1,40 +1,34 @@
 ; A317945: Filter sequence constructed from the coefficients of the Stern polynomials B(d,t) collected for each divisor d of n; Restricted growth sequence transform of A317944.
 ; 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81
 
-sub $3,$$0
-lpb $0,$$0
-  lpb $$6,$$4
-    sub $$0,$$1
+add $1,$$7
+add $1,1
+sub $$6,$7
+sub $$2,$$3
+add $0,$$6
+lpb $$1,$4
+  sub $1,$7
+  add $$1,$$6
+  sub $$4,$$5
+  mov $4,6
+  mov $0,3
+  sub $4,$1
+  mov $$1,$$0
+  add $3,6
+  sub $$7,$$1
+  lpb $$4,3
+    add $2,$0
   lpe
+  add $6,$4
+  mov $3,$$2
+  mov $6,$1
+  add $3,$3
+  sub $5,$$6
+  mov $$7,$5
+  sub $6,$$5
 lpe
-add $1,$0
-add $6,$2
-add $$3,4
-mov $0,5
-add $0,$3
-add $1,$$6
-add $7,$2
-sub $2,$0
-sub $2,$2
-lpb $$2,$$2
-  mov $5,$$3
-lpe
-lpb $1,$7
-  sub $$0,$$1
-lpe
-sub $$0,$$0
-add $7,$5
-sub $2,$3
-sub $1,4
-lpb $$5,$6
-  mov $$0,$4
-  mov $0,$$1
-lpe
-mov $$2,2
-add $$4,$4
-mov $$0,$4
-sub $0,$$5
-mov $0,2
-mov $5,$7
-mov $5,3
-add $$3,6
+mov $5,$$5
+add $$7,$$0
+mov $0,$2
+mov $3,$$2
+mov $6,$$6
