dff12406b68c0638b67c74b5f0109726acfdb0b4
Christian Krause <me@ckrause.org>
2018-10-08 17:18:04 -0700

    new programs

--- programs/oeis/A267883.asm
+++ programs/oeis/A267883.asm
@@ -1,37 +1,16 @@
 ; A267883: Number of OFF (white) cells in the n-th iteration of the "Rule 233" elementary cellular automaton starting with a single ON (black) cell.
 ; 0,3,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
-lpb $$0,$2
-  sub $6,7
-  add $$3,$6
-  add $$4,$$1
+add $5,$$1
+mov $$4,0
+mov $$3,6
+sub $$1,1
+add $1,$$5
+lpb $$4,$$7
+  mov $4,$5
+  add $4,$0
+  sub $$1,1
+  sub $1,2
 lpe
-sub $0,$6
-sub $4,$$0
-add $5,$0
-mov $5,$$6
-mov $$5,4
-sub $5,$4
-mov $7,$$1
-sub $$5,$$6
-add $$6,3
-mov $4,$$0
-mov $2,$7
-sub $2,$1
-add $6,4
-add $1,$$6
-mov $$3,$3
-add $$2,$4
-add $$4,$$3
-mov $3,1
-mov $6,$$6
-sub $3,3
-sub $3,$1
-sub $$2,$$5
-lpb $$4,7
-  mov $$6,$$5
-  add $5,$3
-  sub $$7,$0
-lpe
-mov $5,$4
-sub $4,$$0
+sub $$6,$5
+mov $1,$$1
---
+++ programs/oeis/A272651.asm
@@ -0,0 +1,12 @@
+; A272651: The no-3-in-line problem: maximal number of points from an n X n square grid so that no three lie on a line.
+; 1,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92
+
+add $$5,$$2
+mov $2,$$3
+add $3,$$7
+mov $1,1
+mov $$5,1
+mov $$3,7
+sub $$2,$$2
+add $1,$$4
+add $$0,$3
