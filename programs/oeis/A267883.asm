22e588a79d39c21e5f9c44d63fc33d2781c07d03
Christian Krause <me@ckrause.org>
2018-10-08 21:27:07 -0700

    updated programs

--- programs/oeis/A267883.asm
+++ programs/oeis/A267883.asm
@@ -1,16 +1,14 @@
 ; A267883: Number of OFF (white) cells in the n-th iteration of the "Rule 233" elementary cellular automaton starting with a single ON (black) cell.
 ; 0,3,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
-add $5,$$1
-mov $$4,0
-mov $$3,6
-sub $$1,1
-add $1,$$5
-lpb $$4,$$7
-  mov $4,$5
-  add $4,$0
-  sub $$1,1
-  sub $1,2
+add $$3,3
+lpb $$6,$$7
+  add $5,1
+  add $5,6
+  lpb $2,$$4
+    sub $5,$0
+    sub $$7,$$3
+  lpe
 lpe
-sub $$6,$5
-mov $1,$$1
+mov $1,$5
+mov $2,7
