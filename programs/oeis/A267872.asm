c0a2d8c3c3d11b5af2003751dcecb7c84078d781
Christian Krause <me@ckrause.org>
2018-10-08 20:07:58 -0700

    simplified programs

--- programs/oeis/A267872.asm
+++ programs/oeis/A267872.asm
@@ -1,7 +1,7 @@
 ; A267872: Number of ON (black) cells in the n-th iteration of the "Rule 237" elementary cellular automaton starting with a single ON (black) cell.
 ; 1,1,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,121,123,125
 
-mov $1,$$5
+mov $1,$0
 add $2,1
 mov $4,2
 add $$2,$$7
