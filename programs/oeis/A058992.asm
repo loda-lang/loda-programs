158ab07bf1bb96c212f1645c9490f833bfe85f75
Christian Krause <me@ckrause.org>
2018-10-11 08:30:16 +0200

    new programs

---
+++ programs/oeis/A058992.asm
@@ -0,0 +1,12 @@
+; A058992: Gossip Problem: there are n people and each of them knows some item of gossip not known to the others. They communicate by telephone and whenever one person calls another, they tell each other all that they know at that time. How many calls are required before each gossip knows everything?
+; 0,1,3,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,102,104,106,108,110,112,114,116,118,120,122,124
+
+add $1,$0
+sub $0,1
+add $2,$0
+add $1,$0
+mov $$2,0
+add $$2,4
+mov $0,1
+add $$1,$$2
+sub $$0,1
---
+++ programs/oeis/A230628.asm
@@ -0,0 +1,11 @@
+; A230628: Maximum number of colors needed to color a planar map of several empires, each empire consisting of n countries.
+; 4,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108,114,120,126,132,138,144,150,156,162,168,174,180,186,192,198,204,210,216,222,228,234,240,246,252,258,264,270,276,282,288,294,300,306,312
+
+add $0,$0
+add $0,2
+mov $1,$0
+add $$0,$0
+add $$0,$$0
+mov $2,$1
+add $0,$$0
+add $1,$$2
