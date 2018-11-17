2dc504ce7950205a326bffd1bfa13cb76cf98343
Christian Krause <me@ckrause.org>
2018-11-17 22:00:57 +0100

    new programs

--- programs/oeis/A161365.asm
+++ programs/oeis/A161365.asm
@@ -3,13 +3,11 @@
 
 add $0,1
 lpb $0,1
+  add $1,4
   mov $2,4
-  add $2,4
-  add $1,$3
-  add $2,1
-  sub $0,1
+  sub $0,2
+  mov $4,$2
+  add $1,2
   add $1,$2
-  mov $4,1
-  mov $3,$4
-  sub $0,$4
 lpe
+sub $1,1
--- programs/oeis/A183857.asm
+++ programs/oeis/A183857.asm
@@ -3,10 +3,9 @@
 
 lpb $0,1
   add $1,2
+  sub $1,$0
+  add $4,2
   sub $0,1
-  add $2,$1
-  sub $2,1
-  sub $2,$0
+  add $1,$4
 lpe
-add $1,$2
 add $1,1
