967068ae36ec3fe810a149da6384bab303804cd8
Christian Krause <me@ckrause.org>
2018-11-18 18:03:48 +0100

    updated programs

--- programs/oeis/A010881.asm
+++ programs/oeis/A010881.asm
@@ -3,11 +3,10 @@
 
 add $0,1
 lpb $0,1
-  mov $4,3
   sub $0,1
   mov $1,$0
-  add $4,2
+  mov $3,3
   sub $0,2
-  sub $0,$4
-  sub $0,4
+  sub $0,$3
+  sub $0,6
 lpe
--- programs/oeis/A053644.asm
+++ programs/oeis/A053644.asm
@@ -1,10 +1,9 @@
 ; A053644: Most significant bit of n, msb(n); largest power of 2 less than or equal to n; write n in binary and change all but the first digit to zero.
 ; 0,1,2,2,4,4,4,4,8,8,8,8,8,8,8,8,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,64,64,64,64,64,64,64,64,64,64,64
 
-lpb $0,1
-  add $3,$1
-  mov $1,$3
-  sub $0,1
-  add $1,1
-  sub $0,$3
+add $4,1
+lpb $0,3
+  add $1,$4
+  mov $4,$1
+  sub $0,$4
 lpe
