7b4bc0063b485d1c0effb953069dbd7ecddf082d
Christian Krause <me@ckrause.org>
2018-12-08 16:00:23 +0100

    new programs

---
+++ programs/oeis/A121816.asm
@@ -0,0 +1,16 @@
+; A121816: Conjectured chromatic number of the square of an outerplanar graph G^2 as a function of the maximum degree of a vertex of G.
+; 9,10,11,12,13,14,16,17,19,20,22,23,25,26,28,29,31,32,34,35,37,38,40,41,43,44,46,47,49,50,52,53,55,56,58,59,61,62,64,65,67,68,70,71,73,74,76,77,79,80,82,83,85,86,88,89,91,92,94,95,97,98,100
+
+add $2,$0
+add $1,$2
+add $1,4
+sub $0,4
+add $0,1
+add $1,3
+mov $3,1
+add $1,1
+lpb $0,1
+  sub $0,1
+  add $1,1
+  sub $0,$3
+lpe
