660dfe33612013fc3b0c0a5687385070b4ee2822
Christian Krause <me@ckrause.org>
2018-12-24 10:03:43 +0100

    optimized programs

--- programs/oeis/A057544.asm
+++ programs/oeis/A057544.asm
@@ -1,10 +1,9 @@
 ; A057544: Maximum cycle length (orbit size) in the rotation permutation of n+2 side polygon triangularizations.
 ; 1,1,2,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31
 
-sub $0,1
+mov $2,$0
+add $0,1
+sub $$0,$$0
+sub $$3,$$2
+add $$4,1
 mov $1,$0
-lpb $0,$0
-  mov $0,1
-  add $1,2
-lpe
-add $1,1
