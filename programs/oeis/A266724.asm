f0c81af02e39708268408cfd309957935808ce16
Christian Krause <me@ckrause.org>
2018-12-28 21:16:16 +0100

    optimized programs

--- programs/oeis/A266724.asm
+++ programs/oeis/A266724.asm
@@ -1,14 +1,11 @@
 ; A266724: Number of OFF (white) cells in the n-th iteration of the "Rule 59" elementary cellular automaton starting with a single ON (black) cell.
 ; 0,1,3,1,7,1,11,1,15,1,19,1,23,1,27,1,31,1,35,1,39,1,43,1,47,1,51,1,55,1,59,1,63,1,67,1,71,1,75,1,79,1,83,1,87,1,91,1,95,1,99,1,103,1,107,1,111,1,115,1,119,1,123,1,127,1,131,1,135,1,139,1
 
-mov $2,$0
-lpb $2,1
-  add $1,1
-  sub $4,$1
-  mov $1,$4
-  add $4,1
-  sub $2,1
-  sub $4,$2
-  add $1,$4
-  mov $4,$0
+add $0,$0
+sub $0,1
+mov $1,$0
+lpb $0,2
+  add $$4,1
+  sub $0,5
+  mov $$0,$0
 lpe
--- programs/oeis/A271771.asm
+++ programs/oeis/A271771.asm
@@ -1,16 +1,15 @@
 ; A271771: Maximum total Hamming distance between pairs of consecutive elements in any permutation of all 2^n binary words of length n.
 ; 1,5,18,53,140,347,826,1913,4344,9719,21494,47093,102388,221171,475122,1015793,2162672,4587503,9699310,20447213,42991596,90177515,188743658,394264553,822083560,1711276007,3556769766,7381975013,15300820964,31675383779
 
+add $6,6
+mov $1,1
 mov $2,$0
-add $2,1
-add $0,1
 lpb $2,1
+  add $6,4
+  add $1,$1
   lpb $6,1
-    add $1,$1
-    mov $6,$5
+    add $1,$0
+    sub $6,4
   lpe
-  sub $1,1
-  add $1,$0
-  add $6,5
   sub $2,1
 lpe
