84a17b19118f0fe9d2d4c61eb9f951c0449fc146
Christian Krause <me@ckrause.org>
2018-12-30 11:09:35 +0100

    use get_oeis.sh

--- get_oeis.sh
+++ get_oeis.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-mkdir -p ${HOME}/.loda
+oeis_dir=${HOME}/.loda/oeis
+mkdir -p ${oeis_dir}
 
 for name in stripped names; do
-  if [ -f ${HOME}/.loda/${name} ]; then
-    rm ${HOME}/.loda/${name}
+  if [ -f ${oeis_dir}/${name} ]; then
+    rm ${oeis_dir}/${name}
   fi
-  wget -O ${HOME}/.loda/${name}.gz https://oeis.org/${name}.gz
-  gzip -d ${HOME}/.loda/${name}.gz
+  wget -O ${oeis_dir}/${name}.gz https://oeis.org/${name}.gz
+  gzip -d ${oeis_dir}/${name}.gz
 done
