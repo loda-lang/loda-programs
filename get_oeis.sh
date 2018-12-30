83e016b803fb231a76d97e4ca3b1e37f685c9045
Christian Krause <me@ckrause.org>
2018-12-30 11:28:36 +0100

    download b-files

--- get_oeis.sh
+++ get_oeis.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 oeis_dir=${HOME}/.loda/oeis
-mkdir -p ${oeis_dir}
 
+mkdir -p ${oeis_dir}
 for name in stripped names; do
   if [ -f ${oeis_dir}/${name} ]; then
     rm ${oeis_dir}/${name}
   fi
-  wget -O ${oeis_dir}/${name}.gz https://oeis.org/${name}.gz
+  wget -nv -O ${oeis_dir}/${name}.gz https://oeis.org/${name}.gz
   gzip -d ${oeis_dir}/${name}.gz
+done
+
+mkdir -p ${oeis_dir}/b
+for name in programs/oeis/*.asm; do
+  filename=$(basename -- "$name")
+  aname="${filename%.*}"
+  bname=${aname/A/b}
+  wget -nv -O ${oeis_dir}/b/${bname}.txt https://oeis.org/${aname}/${bname}.txt
 done
