a1b11311a8b12804bb155a93bd18eec27e80ec56
Christian Krause <me@ckrause.org>
2019-01-12 14:14:21 +0100

    push programs

---
+++ push_programs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -d "programs" ]; then
+  echo "programs folder not found; aborting"
+  exit 1
+fi
+
+min_changes=10
+num_changes=$(git status programs -s | wc -l)
+if [ "$num_changes" -ge "$min_changes" ]; then
+  git add programs
+  git commit -m "updated $num_changes programs"
+  git push
+else
+  echo "only $num_changes changes found, but $min_changes required to push"
+fi
