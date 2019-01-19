09bce96c76dfb145d401c976a5aa62d0f2a5a85b
Christian Krause <me@ckrause.org>
2019-01-19 17:11:32 +0100

    new programs

---
+++ programs/oeis/A140438.asm
@@ -0,0 +1,19 @@
+; A140438: Number of letters in word for the number n in Tamil.
+; 1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2
+
+add $6,5
+sub $0,6
+mov $1,2
+add $4,$6
+sub $1,1
+lpb $0,1
+  mov $6,$5
+  add $6,1
+  mov $0,$1
+  sub $0,$4
+  add $3,$6
+  add $0,4
+  mov $2,$3
+  add $1,$2
+  sub $0,1
+lpe
