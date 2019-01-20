eac96937b6c99de8410fb11c20ac17a0c6535cb5
Christian Krause <me@ckrause.org>
2019-01-20 12:18:27 +0100

    bug fixes and new programs

---
+++ programs/oeis/A029865.asm
@@ -0,0 +1,13 @@
+; A029865: Smallest covering radius of [ n,6 ] binary code.
+; 0,1,1,1,2,2,3,3,3,4,4
+
+mov $4,$0
+add $3,$4
+lpb $0,1
+  add $2,3
+  sub $3,$2
+  sub $2,$3
+  sub $0,$2
+  sub $0,1
+  add $1,1
+lpe
