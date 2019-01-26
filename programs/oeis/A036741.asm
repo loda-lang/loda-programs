850e82c8fb8c9ff84faa00901fa5bd8b11b2ac03
Christian Krause <me@ckrause.org>
2019-01-27 00:00:07 +0100

    updated 34 programs

--- programs/oeis/A036741.asm
+++ programs/oeis/A036741.asm
@@ -1,15 +1,11 @@
 ; A036741: Values increase, Roman numerals increase lexicographically.
 ; 1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,20,21,22,23,24,25,26,27,28,30,31,32,33,34,35,36,37,38
 
-add $2,$0
-add $1,$2
-sub $0,2
-add $0,4
+mov $2,$0
+mov $1,$2
+add $0,2
 lpb $0,1
   sub $0,6
-  mov $3,5
   add $1,1
-  add $0,3
-  sub $0,$3
-  sub $0,1
+  sub $0,3
 lpe
