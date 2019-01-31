4a264a96feab474343862930fb66634c5de5f42c
Christian Krause <me@ckrause.org>
2019-02-01 00:00:07 +0100

    updated 33 programs

--- programs/oeis/A027004.asm
+++ programs/oeis/A027004.asm
@@ -4,9 +4,8 @@
 add $2,6
 add $1,1
 lpb $0,1
+  add $2,$1
+  add $1,$2
+  add $2,3
   sub $0,1
-  add $3,$2
-  add $3,$1
-  add $1,$3
-  sub $3,3
 lpe
---
+++ programs/oeis/A137325.asm
@@ -0,0 +1,21 @@
+; A137325: Number of terms in the Janet periodic table of the elements 32 columns: ordered 14 2's, 10 4's, 6 6's, 2 8's.
+; 2,2,2,2,2,2,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,4,4,4,6,6,6,6,6,6,8,8
+
+add $5,2
+sub $0,2
+mov $4,4
+add $6,7
+add $0,$4
+sub $0,$5
+mov $3,$5
+lpb $0,1
+  sub $0,$6
+  add $1,$3
+  sub $0,5
+  add $6,1
+  sub $6,$2
+  mov $2,$3
+  sub $0,1
+  add $6,$2
+  sub $6,6
+lpe
