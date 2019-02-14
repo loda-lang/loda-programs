fa39a79716b8e81b359ccd4bebb48a789160a861
Christian Krause <me@ckrause.org>
2019-02-15 00:00:07 +0100

    updated 63 programs

---
+++ programs/oeis/A267847.asm
@@ -0,0 +1,18 @@
+; A267847: Decimal representation of the n-th iteration of the "Rule 227" elementary cellular automaton starting with a single ON (black) cell.
+; 1,4,26,122,506,2042,8186,32762,131066,524282,2097146,8388602,33554426,134217722,536870906,2147483642,8589934586,34359738362,137438953466,549755813882,2199023255546,8796093022202,35184372088826,140737488355322,562949953421306
+
+add $1,$0
+add $0,$1
+add $3,1
+lpb $0,1
+  sub $0,1
+  add $3,$3
+  add $1,3
+  mov $4,$1
+  mov $1,$0
+lpe
+add $2,$3
+sub $3,2
+sub $3,$4
+add $3,$2
+add $1,$3
---
+++ programs/oeis/A314838.asm
@@ -0,0 +1,23 @@
+; A314838: Coordination sequence Gal.4.52.3 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+; 1,5,9,14,18,23,27,32,37,41,46,50,55,59,64,69,73,78,82,87,91,96,101,105,110,114,119,123,128,133,137,142,146,151,155,160,165,169,174,178,183,187,192,197,201,206,210,215,219,224
+
+mov $2,$0
+add $0,$0
+add $5,$0
+add $2,$2
+mov $4,$5
+sub $5,4
+add $0,1
+add $1,$2
+add $0,$5
+lpb $0,1
+  sub $0,1
+  mov $3,4
+  add $4,3
+  add $1,$3
+  sub $0,6
+  sub $4,3
+  sub $3,1
+  sub $1,$3
+lpe
+add $1,$4
