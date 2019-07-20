469ada18893e766c129fb102f3046118f8eed06c
Christian Krause <me@ckrause.org>
2019-07-20 20:39:57 +0200

    some linear programs (to be tested)

---
+++ programs/oeis/A317592.asm
@@ -0,0 +1,5 @@
+; A317592: Lexicographically first sequence of different terms such that erasing the last two digits of a(n+1) and adding this new reshaped integer to a(n) gives back a(n+1).
+; 99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158
+
+mov $1,$0
+add $1,99
