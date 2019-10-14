5824467ab5e93bf5a76718dc914bd2cd724e7bd4
Christian Krause <me@ckrause.org>
2019-10-14 21:09:20 +0200

    optimized linear programs

--- programs/oeis/A136293.asm
+++ programs/oeis/A136293.asm
@@ -1,9 +1,8 @@
 ; A136293: Linear bound on the genera of Heegaard splittings of closed, orientable 3-manifolds that admit a generalized triangulation with n generalized tetrahedra.
 ; 26,102,178,254,330,406,482,558,634,710,786,862,938,1014,1090,1166,1242,1318,1394,1470,1546,1622,1698,1774,1850,1926,2002,2078,2154,2230,2306,2382,2458,2534,2610,2686,2762,2838,2914,2990,3066,3142,3218,3294,3370
 
-mov $2,$0
-lpb $2,1
+mov $1,26
+lpb $0,1
   add $1,76
-  sub $2,1
+  sub $0,1
 lpe
-add $1,26
