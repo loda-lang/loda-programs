5e9baad1d5681e54b461531ab3b0be2e5033a034
Christian Krause <me@ckrause.org>
2019-12-04 19:55:12 +0100

    synthesized 526 shorter linear programs

--- programs/oeis/A105734.asm
+++ programs/oeis/A105734.asm
@@ -1,19 +1,14 @@
 ; A105734: For n>2, a(n) > 0 is such that a(n-1)^2+4*a(n-2)*a(n) is a minimal square, with a(1)=1, a(2)=1.
 ; 1,1,2,3,2,1,1,2,3,2,1,1,2,3,2,1,1,2,3,2,1,1
 
-lpb $0,1
-  add $3,$0
-  sub $0,3
-  sub $3,$0
-  add $3,3
-  sub $3,$0
-  sub $3,3
-  mov $2,$1
-  add $1,$3
-  add $2,2
-  add $1,$2
-  sub $0,1
-  sub $1,3
-  sub $0,1
+mov $6,$0
+lpb $2,$6
+  sub $6,5
 lpe
-add $1,1
+mov $2,$6
+add $2,3
+mov $3,1
+mov $4,1
+mov $5,2
+mov $7,2
+mov $1,$$2
