bdfc89217bc194a884958ca561ef9ad9ee846fc8
Christian Krause <me@ckrause.org>
2020-08-19 21:48:43 +0200

    updated 26 programs

--- programs/oeis/140/A140716.asm
+++ programs/oeis/140/A140716.asm
@@ -1,12 +1,9 @@
 ; A140716: Blocky integers, i.e., integers n > 1 such that there is a run of n consecutive integer squares the average of which is a square.
 ; 7,25,31,49,55,73,79,97,103,121,127,145,151,169,175,193,199,217,223,241,247,265,271,289,295,313,319,337,343,361,367,385,391,409,415,433,439,457,463,481,487,505,511,529,535,553,559,577,583
 
-mov $2,$0
-add $0,1
-mov $1,$2
-add $1,$0
-gcd $0,2
-add $1,1
+mov $1,$0
+add $0,$1
+mod $1,2
 add $1,$0
 mul $1,6
-sub $1,11
+add $1,7
