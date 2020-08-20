4574dd6117fa86aa40e46393dbf776f6902dae66
Christian Krause <me@ckrause.org>
2020-08-20 15:52:50 +0200

    updated 51 programs

--- programs/oeis/204/A204458.asm
+++ programs/oeis/204/A204458.asm
@@ -5,20 +5,8 @@ mov $1,$0
 div $1,8
 add $1,1
 add $0,1
-mov $2,1
 div $1,2
-lpb $0,5
-  mov $2,$0
-  lpb $2,2
-    sub $0,$2
-    pow $1,4
-  lpe
-lpe
-mov $2,0
-mul $2,2
 add $1,$0
-add $1,1
-mov $2,$1
-sub $1,2
+sub $1,1
 mul $1,2
 add $1,1
