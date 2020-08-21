8993fb02d8c924746304c6a873656dd036b717ec
Christian Krause <me@ckrause.org>
2020-08-21 17:13:45 +0200

    updated 7 programs

--- programs/oeis/288/A288699.asm
+++ programs/oeis/288/A288699.asm
@@ -1,12 +1,16 @@
 ; A288699: Decimal representation of the diagonal from the corner to the origin of the n-th stage of growth of the two-dimensional cellular automaton defined by "Rule 494", based on the 5-celled von Neumann neighborhood.
 ; 1,1,0,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2
 
+mov $3,1
+mov $2,1
+add $2,$0
 sub $0,1
-mov $2,$0
-mov $1,$2
-add $1,1
+sub $3,$0
 lpb $0,1
-  sub $1,2
-  mov $0,$1
-  sub $0,2
+  mod $2,2
+  mov $1,1
+  add $2,$1
+  mov $3,$2
+  mov $0,1
 lpe
+mov $1,$3
