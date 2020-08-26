509fb9b569b956bc968f6e9f59f05f7b90250ca4
Christian Krause <me@ckrause.org>
2020-08-26 08:27:27 +0200

    updated 30 programs

--- programs/oeis/329/A329678.asm
+++ programs/oeis/329/A329678.asm
@@ -1,14 +1,11 @@
 ; A329678: Number of excursions of length n with Motzkin-steps consisting only of consecutive steps UD and DH.
 ; 1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
-mov $3,2
-mov $2,$0
-lpb $2,1
-  mov $3,0
-  mov $1,1
-  mul $1,3
-  mov $2,$1
+mov $1,8
+mov $2,1
+lpb $1,1
+  sub $1,10
+  mul $1,$0
 lpe
-mul $3,2
-mov $1,$3
-div $1,4
+clr $0,$1
+mov $1,$2
