b20025ffc5bf09ca021e246d58feb3251a373f2e
Christian Krause <me@ckrause.org>
2020-08-28 17:47:23 +0200

    updated programs

--- programs/oeis/300/A300404.asm
+++ programs/oeis/300/A300404.asm
@@ -1,13 +1,12 @@
 ; A300404: Smallest integer k such that the largest term in the Goodstein sequence starting at k is > n.
 ; 2,2,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
 
-bin $0,2
-mov $3,$0
-mov $2,$3
+mov $2,$0
+pow $0,2
 lpb $0,1
-  mov $2,1
-  mul $2,2
-  mov $0,1
+  mov $2,5
+  mov $0,5
 lpe
+div $2,2
 mov $1,$2
 add $1,2
