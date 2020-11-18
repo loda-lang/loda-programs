d2f8011c8a17a31099a23c27517be0002f845883
Christian Krause <me@ckrause.org>
2020-11-18 19:57:44 +0100

    updated 8 programs

--- programs/oeis/189/A189980.asm
+++ programs/oeis/189/A189980.asm
@@ -1,8 +1,7 @@
 ; A189980: a(n) is the number of incongruent two-color bracelets of n beads, 10 from them are black (A005515), having a diameter of symmetry.
 ; 1,1,6,6,21,21,56,56,126,126,252,252,462,462,792,792,1287,1287,2002,2002,3003,3003,4368,4368,6188,6188,8568,8568,11628,11628,15504,15504,20349,20349,26334,26334,33649,33649
 
-add $0,10
-mov $2,$0
-div $2,2
-bin $2,5
-mov $1,$2
+div $0,2
+add $0,5
+bin $0,5
+mov $1,$0
