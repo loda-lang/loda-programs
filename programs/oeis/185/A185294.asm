74d4f88d23bc1c3ab866749957cca9eba1b153d4
Christian Krause <me@ckrause.org>
2020-11-28 00:02:11 +0100

    updated 25 programs

--- programs/oeis/185/A185294.asm
+++ programs/oeis/185/A185294.asm
@@ -1,18 +1,15 @@
 ; A185294: Number of disconnected 9-regular simple graphs on 2n vertices with girth at least 4.
 ; 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,15
 
-sub $0,6
+div $0,2
+trn $0,5
+pow $0,2
+trn $0,10
 lpb $0,1
-  mov $1,$0
-  div $1,2
-  sub $1,5
-  mov $2,8
-  pow $2,$1
-  mov $3,$2
-  pow $6,$5
-  gcd $0,$6
-  add $4,$3
+  add $0,3
+  pow $0,2
+  sub $0,2
+  div $0,2
+  sub $0,38
 lpe
-trn $4,4
-mov $1,$4
-div $1,4
+mov $1,$0
