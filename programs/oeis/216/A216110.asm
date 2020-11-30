d8dfc23371b0a9a02f262aa967fb9faca25e3cf5
Christian Krause <me@ckrause.org>
2020-11-30 18:28:54 +0100

    updated 23 programs

--- programs/oeis/216/A216110.asm
+++ programs/oeis/216/A216110.asm
@@ -1,18 +1,17 @@
 ; A216110: The Wiener index of the meta-polyphenyl chain with n hexagons (see the Dou et al. and the Deng references).
 ; 27,198,621,1404,2655,4482,6993,10296,14499,19710,26037,33588,42471,52794,64665,78192,93483,110646,129789,151020,174447,200178,228321,258984,292275,328302,367173,408996,453879,501930
 
-mov $3,$0
-mov $2,$3
-add $3,2
-pow $3,2
-mov $4,2
-mul $4,$2
-mov $5,$2
-add $5,$2
-add $5,$4
-mul $3,$5
-add $3,$4
+mov $3,2
+add $3,$0
+add $0,$3
+pow $0,2
+mul $0,$3
+mul $3,2
+sub $3,$0
+sub $3,36
 mov $1,$3
+sub $2,$1
+mov $1,$2
 div $1,2
+sub $1,17
 mul $1,9
-add $1,27
