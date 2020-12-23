36867cd1198776678c962516278274d9c6cc9a6d
Christian Krause <me@ckrause.org>
2020-12-23 19:23:03 +0100

    updated 47 programs

--- programs/oeis/084/A084506.asm
+++ programs/oeis/084/A084506.asm
@@ -5,7 +5,6 @@ mul $0,2
 lpb $0,1
   add $0,3
   div $0,4
-  add $2,1
+  add $1,1
 lpe
-mov $1,$2
 add $1,1
--- programs/oeis/126/A126269.asm
+++ programs/oeis/126/A126269.asm
@@ -1,11 +1,10 @@
 ; A126269: Numbers n such that hcl(n,n) < hcl(n,n-1) where hcl(n,i) is the Huffman code length; see comments.
 ; 3,4,9,10,21,22,45,46,93,94,189,190,381,382,765,766,1533,1534,3069,3070,6141,6142,12285,12286,24573,24574,49149,49150
 
-mov $2,6
+mov $1,6
 lpb $0,1
   sub $0,2
-  mul $2,2
+  mul $1,2
 lpe
-add $0,$2
-mov $1,$0
+add $1,$0
 sub $1,3
