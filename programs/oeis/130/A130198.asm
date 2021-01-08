962679a9c350704bdb0ba82d69ed655ff51b32a6
Christian Krause <me@ckrause.org>
2021-01-08 09:15:21 +0100

    updated 59 programs

--- programs/oeis/130/A130198.asm
+++ programs/oeis/130/A130198.asm
@@ -1,9 +1,8 @@
 ; A130198: Single paradiddle. In percussion, the paradiddle is a four-note drum sticking pattern consisting of two alternating notes followed by two notes on the same hand.
 ; 0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1
 
-mov $1,6
-mul $1,$0
-div $1,4
-add $1,1
-div $1,2
-mod $1,2
+mul $0,5
+bin $0,2
+bin $0,2
+mod $0,2
+mov $1,$0
