2bf8355ca177793c1fbe4189f49703501b6e0b41
Christian Krause <me@ckrause.org>
2021-01-23 13:11:39 +0100

    updated 189 programs

--- programs/oeis/072/A072690.asm
+++ programs/oeis/072/A072690.asm
@@ -3,21 +3,10 @@
 
 lpb $0,1
   mov $1,$0
-  mov $3,$0
-  mov $1,$0
   cal $1,20703
-  mov $3,1
   sub $1,$0
-  sub $1,$3
-  add $2,$1
-  mov $3,$0
-  mul $1,$1
-  mov $1,$3
   sub $0,1
+  sub $1,1
+  add $2,$1
 lpe
 mov $1,$2
-mov $3,1
-sub $0,4
-mov $4,$3
-mul $0,2
-mov $3,$0
---
+++ programs/oeis/198/A198447.asm
@@ -0,0 +1,15 @@
+; A198447: Number of 2n X 2 0..2 arrays with values 0..2 introduced in row major order and each element unequal to exactly two horizontal and vertical neighbors.
+; 3,13,71,433,2763,17941,117263,768313,5038611,33054493,216872663,1422982081,9336876123,61264171813,401987528351,2637661006153,17307148601763,113561761317421,745141474228583,4889285086978513
+
+lpb $0,1
+  mov $1,$0
+  mul $1,2
+  cal $1,6131
+  sub $0,1
+  add $2,$1
+lpe
+add $2,1
+mov $1,$2
+sub $1,1
+mul $1,2
+add $1,3
