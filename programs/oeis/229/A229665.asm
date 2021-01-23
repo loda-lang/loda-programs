6fa18218994b852bb623a4753e898186b23cecb7
Christian Krause <me@ckrause.org>
2021-01-23 01:16:05 +0100

    updated 26 programs

---
+++ programs/oeis/229/A229665.asm
@@ -0,0 +1,25 @@
+; A229665: Number of defective 4-colorings of an nX1 0..3 array connected horizontally, antidiagonally and vertically with exactly two mistakes, and colors introduced in row-major 0..3 order
+; 0,0,1,3,12,50,210,861,3416,13140,49230,180455,649572,2302950,8060234,27900705,95659440,325241960,1097691462,3680494731,12268315580,40679151450,134241199554,441078226853,1443528742152,4707158941500
+
+mov $2,2
+mov $3,$0
+cal $3,73424
+mov $2,1
+mov $1,$0
+pow $3,2
+mov $1,$0
+mov $2,$0
+sub $0,1
+mov $1,$0
+mov $1,$2
+cal $0,229472
+add $4,2
+mul $0,$1
+mov $3,$2
+sub $1,2
+mov $1,$0
+add $1,1
+sub $3,$1
+pow $1,0
+mov $1,$0
+div $1,2
