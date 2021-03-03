db2e204bd64de800e05df4731063b8b643fbd48b
Christian Krause <me@ckrause.org>
2021-03-03 15:20:08 +0100

    updated 23/25506 programs

--- programs/oeis/341/A341389.asm
+++ programs/oeis/341/A341389.asm
@@ -1,6 +1,10 @@
 ; A341389: Characteristic function of A158705, nonnegative integers with an odd number of even powers of 2 in their base-2 representation.
 ; 0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,1,0,1,0,0,1,0,1,1,0
 
-cal $0,112539 ; Half-baked Thue-Morse: at successive steps the sequence or its bit-inverted form is appended to itself.
-add $1,$0
-cmp $1,0
+mov $1,9
+lpb $0,1
+  add $1,$0
+  div $0,4
+lpe
+gcd $1,2
+sub $1,1
