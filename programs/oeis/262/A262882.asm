457bc8b47ba5698d4ce94aa474bc4a007a8062a5
Christian Krause <me@ckrause.org>
2021-04-16 21:27:06 +0200

    updated 14 programs

--- programs/oeis/262/A262882.asm
+++ programs/oeis/262/A262882.asm
@@ -3,7 +3,7 @@
 
 lpb $0
   mov $1,$0
-  cal $1,37834 ; Sum{|d(i)-d(i-1)|: i=1,...,m}, where Sum{d(i)*2^i: i=0,1,...,m} is base 2 representation of n.
+  cal $1,37834 ; a(n) = Sum_{i=1..m} |d(i) - d(i-1)|, where Sum_{i=0..m} d(i)*2^i is the base-2 representation of n.
   sub $0,1
   sub $1,2
   sub $0,$1
