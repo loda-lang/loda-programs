ee5a1199cecf40c46ddd766cf23913e8e6ac7cb8
Christian Krause <me@ckrause.org>
2021-04-18 16:15:27 +0200

    adaptive scheduler (#118)

--- programs/oeis/186/A186685.asm
+++ programs/oeis/186/A186685.asm
@@ -1,9 +1,8 @@
 ; A186685: Total number of n-digit numbers requiring 19 positive biquadrates in their representation as sum of biquadrates.
 ; 0,1,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
-mov $13,$0
-mul $13,2
-bin $13,2
-mov $2,$13
-clr $4,$2
-mov $1,$13
+sub $0,1
+mul $0,6
+bin $0,6
+mov $1,6
+bin $1,$0
