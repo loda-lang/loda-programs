f2a632f77e702efcda5128cb8c1a46fef22e3ce6
Christian Krause <me@ckrause.org>
2021-04-28 20:38:28 +0200

    fix recursions

--- programs/oeis/262/A262247.asm
+++ programs/oeis/262/A262247.asm
@@ -1,8 +1,9 @@
 ; A262247: Number of squares formed from a square composed of p^2 unit squares where p is n-th prime.
 ; 5,14,55,140,506,819,1785,2470,4324,8555,10416,17575,23821,27434,35720,51039,70210,77531,102510,121836,132349,167480,194054,238965,308945,348551,369564,414090,437635,487369,690880,757966,866525,904890,1113775,1159076
 
-cal $0,98996 ; p(p+1)(2p+1) where p is prime.
-add $1,$0
-sub $1,30
-div $1,6
+cal $0,40 ; The prime numbers.
+cal $0,2492 ; Sum of the first n even squares: 2*n*(n+1)*(2*n+1)/3.
+mov $1,$0
+sub $1,20
+div $1,4
 add $1,5
