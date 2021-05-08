1754ec8fa20e05cc8de70def810a2cc14fd261ea
Christian Krause <me@ckrause.org>
2021-05-08 03:17:12 +0200

    updated 25/28004 programs

--- programs/oeis/188/A188037.asm
+++ programs/oeis/188/A188037.asm
@@ -3,6 +3,5 @@
 
 sub $0,1
 max $0,0
-cal $0,276862 ; First differences of the Beatty sequence A003151 for 1 + sqrt(2).
+cal $0,159684 ; Sturmian word: limit S(infinity) where S(0) = 0, S(1) = 0,1 and for n>=1, S(n+1) = S(n)S(n)S(n-1).
 mov $1,$0
-sub $1,2
