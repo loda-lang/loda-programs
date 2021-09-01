fd2aa2d6d36aee8037c81b7620a508a842b9a9fa
Simon Strandgaard <neoneye@gmail.com>
2021-09-01 12:58:27 +0200

    Marginally faster version of A000796. 4 steps faster in every step.

--- oeis/000/A000796.asm
+++ oeis/000/A000796.asm
@@ -1,6 +1,30 @@
 ; A000796: Decimal expansion of Pi (or digits of Pi).
+; Coded manually by Simon Strandgaard, by modifying the A059833: "Madonna's Sequence".
 ; 3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8,8,4,1,9,7,1,6,9,3,9,9,3,7,5,1,0,5,8,2,0,9,7,4,9,4,4,5,9,2,3,0,7,8,1,6,4,0,6,2,8,6,2,0,8,9,9,8,6,2,8,0,3,4,8,2,5,3,4,2,1,1,7,0,6,7
 
-seq $0,59833 ; "Madonna's Sequence": add 1 (mod 10) to each digit of Pi.
-add $0,9
+mov $1,1
+mov $2,1
+mov $3,$0
+mul $3,5
+lpb $3
+  mul $1,$3
+  mov $5,$3
+  mul $5,2
+  add $5,1
+  mul $2,$5
+  add $1,$2
+  cmp $4,0
+  mov $5,$0
+  add $5,$4
+  div $1,$5
+  div $2,$5
+  sub $3,1
+lpe
+mul $1,2
+mov $6,10
+pow $6,$0
+div $2,$6
+div $1,$2
+add $1,$6
+mov $0,$1
 mod $0,10
