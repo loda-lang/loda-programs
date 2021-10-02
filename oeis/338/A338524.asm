d13b6f559fd5ac93184c10b98884a739c0dc30fc
Simon Strandgaard <neoneye@gmail.com>
2021-10-02 17:34:57 +0200

    First version

---
+++ oeis/338/A338524.asm
@@ -0,0 +1,6 @@
+; A338524: prime(n) Gray code decoding.
+; Submitted by Simon Strandgaard
+; 3,2,6,5,13,9,30,29,26,22,21,57,49,50,53,38,45,41,125,122,113,117,98,110,65,70,69,77,73,94,85,253,241,242,230,229,233,194,197,201
+
+seq $0,40 ; The prime numbers.
+seq $0,6068 ; a(n) is Gray-coded into n. 
