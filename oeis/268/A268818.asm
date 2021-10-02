193456b86a33ab31eb1400c4fb039cc368671cd4
Simon Strandgaard <neoneye@gmail.com>
2021-10-02 21:48:38 +0200

    First version

---
+++ oeis/268/A268818.asm
@@ -0,0 +1,7 @@
+; A268818: Permutation of nonnegative integers: a(n) = A268718(A268718(n)).
+; Submitted by Simon Strandgaard
+; 0,1,6,4,3,10,2,7,15,5,14,11,8,13,18,16,23,29,22,19,32,21,9,24,12,25,30,28,27,34,26,31,39,45,38,35,48,37,57,40
+
+seq $0,268718 ; Permutation of natural numbers: a(0) = 0, a(n) = 1 + A003188(A006068(n)-1), where A003188 is binary Gray code and A006068 is its inverse.
+seq $0,268718 ; Permutation of natural numbers: a(0) = 0, a(n) = 1 + A003188(A006068(n)-1), where A003188 is binary Gray code and A006068 is its inverse.
+
