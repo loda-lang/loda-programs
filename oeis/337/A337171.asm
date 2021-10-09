d9f5f0dd459969e5167537784b25644f3afd24e9
Simon Strandgaard <neoneye@gmail.com>
2021-10-09 16:21:32 +0200

    First version

---
+++ oeis/337/A337171.asm
@@ -0,0 +1,8 @@
+; A337171: a(n) = A004186(n) mod n.
+; Submitted by Simon Strandgaard
+; 0,0,0,0,0,0,0,0,0,0,0,9,5,13,6,13,3,9,15,0,0,0,9,18,2,10,18,26,5,0,0,0,0,9,18,27,36,7,15,0
+
+add $0,1
+mov $1,$0
+seq $0,4186 ; Arrange digits of n in decreasing order.
+mod $0,$1
