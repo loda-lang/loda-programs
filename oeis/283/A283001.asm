4ca4aec26f535a2e99885cdcfbeabc54175acc1d
Simon Strandgaard <neoneye@gmail.com>
2021-10-09 16:24:30 +0200

    First version

---
+++ oeis/283/A283001.asm
@@ -0,0 +1,8 @@
+; A283001: a(n) = (A004186(n) - n)/9.
+; Submitted by Simon Strandgaard
+; 0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,0,0,0,1,2,3,4,5,6,7,0,0,0,0,1,2,3,4,5,6
+
+mov $1,$0
+seq $0,4186 ; Arrange digits of n in decreasing order.
+sub $0,$1
+div $0,9
