daa44e5030387d70f8c8545fad8e664cd824725b
Simon Strandgaard <neoneye@gmail.com>
2021-10-15 16:55:50 +0200

    First version

---
+++ oeis/315/A315518.asm
@@ -0,0 +1,13 @@
+; A315518: Coordination sequence Gal.5.291.4 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+; Submitted by Simon Strandgaard
+; 1,6,11,17,22,27,32,37,43,48,54,60,65,71,76,81,86,91,97,102,108,114,119,125,130,135,140,145,151,156,162,168,173,179,184,189,194,199,205,210
+
+mov $1,$0
+mov $2,$0
+seq $1,314164 ; Coordination sequence Gal.5.302.2 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+mul $0,7
+sub $0,1
+mod $0,$1
+add $0,1
+mul $2,4
+add $0,$2
