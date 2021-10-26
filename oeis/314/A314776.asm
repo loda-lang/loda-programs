3f64e3a8d63a331f8fc019f36892f70b19cb5886
Simon Strandgaard <neoneye@gmail.com>
2021-10-26 18:04:11 +0200

    First version

---
+++ oeis/314/A314776.asm
@@ -0,0 +1,11 @@
+; A314776: Coordination sequence Gal.6.478.4 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+; Submitted by Simon Strandgaard
+; 1,5,9,13,20,23,27,31,40,41,45,49,60,59,63,67,80,77,81,85,100,95,99,103,120,113,117,121,140,131,135,139,160,149,153,157,180,167,171,175
+
+mov $1,$0
+seq $0,315237 ; Coordination sequence Gal.4.128.4 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+add $0,5
+div $0,2
+sub $0,2
+mul $1,2
+add $0,$1
