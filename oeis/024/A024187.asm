8fc645bbf6d2e33208310b7333c31cdd32fb335a
Christian Krause <me@ckrause.org>
2021-10-28 21:24:47 +0200

    updated 16 programs

--- oeis/024/A024187.asm
+++ oeis/024/A024187.asm
@@ -1,9 +1,7 @@
 ; A024187: n-th elementary symmetric function of 3,4,...,n+3.
-; Submitted by Jamie Morken
+; Submitted by Christian Krause
 ; 7,47,342,2754,24552,241128,2592720,30334320,383970240,5231113920,76349105280,1188825724800,19675048780800
 
 add $0,2
 seq $0,122057 ; a(n) = (n+1)! * (H(n+1) - H(2)), where H(n) are the harmonic numbers.
-sub $0,14
 div $0,2
-add $0,7
--- oeis/099/A099157.asm
+++ oeis/099/A099157.asm
@@ -1,6 +1,19 @@
 ; A099157: a(n) = 4^(n-1)*U(n-1, 3/2) where U is the Chebyshev polynomial of the second kind.
+; Submitted by Jamie Morken(s1)
 ; 0,1,12,128,1344,14080,147456,1544192,16171008,169345024,1773404160,18571329536,194481487872,2036636581888,21327935176704,223349036810240,2338941478895616,24493713157783552,256501494231072768
 
 mul $0,2
-seq $0,163305 ; Numerators of fractions in the approximation of the square root of 5 satisfying: a(n)= (a(n-1)+ c)/(a(n-1)+1); with c=5 and a(1)=0. Also product of the powers of two and five times the Fibonacci numbers.
-div $0,10
+mov $3,1
+lpb $0
+  sub $0,1
+  add $3,$1
+  mov $2,$3
+  mul $2,2
+  add $3,$1
+  mov $1,$2
+  sub $1,$3
+  mul $3,2
+lpe
+mov $0,$1
+mul $0,2
+div $0,4
