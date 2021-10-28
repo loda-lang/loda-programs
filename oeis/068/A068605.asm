ce74d9eef890dae627b337736c53eac64d540ea9
Christian Krause <me@ckrause.org>
2021-10-28 17:49:59 +0200

    updated programs

--- oeis/068/A068605.asm
+++ oeis/068/A068605.asm
@@ -1,14 +1,14 @@
 ; A068605: Number of functions from [1,2,...,n] to [1,2,...,n] such that the image contains exactly two elements.
+; Submitted by Jamie Morken(s1)
 ; 2,18,84,300,930,2646,7112,18360,45990,112530,270204,638820,1490762,3440430,7864080,17825520,40107726,89652906,199229060,440401500,968883762,2122317318,4630511064,10066329000,21810380150,47110421826
 
 add $0,2
-mul $0,2
-sub $0,2
-lpb $0
-  add $1,$0
-  sub $0,2
-  mul $2,2
-  add $2,1
-lpe
+mov $1,2
+mov $2,$0
+sub $0,1
+pow $1,$0
+bin $2,2
 mul $1,$2
+sub $1,$2
 mov $0,$1
+mul $0,2
