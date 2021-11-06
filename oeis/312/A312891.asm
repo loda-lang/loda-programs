b4453e5a1ddde39cab7d0c9aeb9d35d8a17c91fa
Christian Krause <me@ckrause.org>
2021-11-06 13:54:57 +0100

    updated 60 programs

--- oeis/312/A312891.asm
+++ oeis/312/A312891.asm
@@ -1,10 +1,15 @@
 ; A312891: Coordination sequence Gal.6.216.5 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+; Submitted by Jamie Morken(s1)
 ; 1,4,9,13,17,21,25,29,33,37,42,46,50,55,59,63,67,71,75,79,83,88,92,96,101,105,109,113,117,121,125,129,134,138,142,147,151,155,159,163,167,171,175,180,184,188,193,197,201,205
 
-mul $0,3
-seq $0,311225 ; Coordination sequence Gal.6.216.1 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
-add $1,$0
-sub $1,2
-div $1,3
+mul $0,2
+mov $1,$0
+mul $0,26
+add $0,5
+div $0,11
+mul $1,49
+sub $1,6
+div $1,11
 add $1,1
+sub $1,$0
 mov $0,$1
