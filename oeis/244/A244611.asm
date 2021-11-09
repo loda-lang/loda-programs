0c4fa371965b1987fdc6cc514652d479f1d09d5a
Simon Strandgaard <neoneye@gmail.com>
2021-11-09 21:36:05 +0100

    Eliminated indirect memory access.

--- oeis/244/A244611.asm
+++ oeis/244/A244611.asm
@@ -5,6 +5,10 @@
 seq $0,300518 ; The greatest prime factor of the squarefree part of n, or 1 if n is square.
 trn $0,2
 add $0,1
-pow $1,0
-mov $2,-1
-mov $$3,$$0
+mov $3,$0
+mov $4,$0
+cmp $3,1
+cmp $4,2
+mov $0,0
+add $0,$3
+sub $0,$4
