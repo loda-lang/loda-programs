aee61bb171a18eceb061522d6288ed1846498271
Christian Krause <me@ckrause.org>
2021-11-11 20:23:54 +0100

    updated programs

--- oeis/027/A027660.asm
+++ oeis/027/A027660.asm
@@ -1,6 +1,13 @@
 ; A027660: a(n) = C(n+2,2) + C(n+2,3) + C(n+2,4) + C(n+2,5).
+; Submitted by Jamie Morken(s3)
 ; 1,4,11,26,56,112,210,372,627,1012,1573,2366,3458,4928,6868,9384,12597,16644,21679,27874,35420,44528,55430,68380,83655,101556,122409,146566,174406,206336,242792
 
+add $0,3
+mov $1,$0
+bin $0,5
+bin $1,3
+add $0,$1
+mul $0,8
+sub $0,8
+div $0,8
 add $0,1
-seq $0,227252 ; Number of n X 2 binary arrays indicating whether each 2 X 2 subblock of a larger binary array has lexicographically nondecreasing rows and columns, for some larger (n+1) X 3 binary array with rows and columns of the latter in lexicographically nondecreasing order.
-div $0,2
--- oeis/037/A037654.asm
+++ oeis/037/A037654.asm
@@ -1,5 +1,5 @@
 ; A037654: Base 5 digits are, in order, the first n terms of the periodic sequence with initial period 3,0,2.
-; Submitted by Jon Maiga
+; Submitted by Jamie Morken(s4)
 ; 3,15,77,388,1940,9702,48513,242565,1212827,6064138,30320690,151603452,758017263,3790086315,18950431577,94752157888,473760789440,2368803947202,11844019736013,59220098680065,296100493400327
 
 mov $2,3
@@ -9,9 +9,8 @@ lpb $0
   add $1,$2
   add $1,1
   mul $1,5
-  add $2,6
-  mul $2,2
-  div $2,4
+  add $2,2
+  max $2,2
   mod $2,4
 lpe
 add $1,$2
