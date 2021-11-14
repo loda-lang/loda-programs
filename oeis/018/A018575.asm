6411766b90b4a82486f449e1a33cf4ed4efa7986
Christian Krause <me@ckrause.org>
2021-11-14 14:48:42 +0100

    added 136 programs

---
+++ oeis/018/A018575.asm
@@ -0,0 +1,13 @@
+; A018575: Divisors of 656.
+; Submitted by Jon Maiga
+; 1,2,4,8,16,41,82,164,328,656
+
+mov $3,1
+lpb $0
+  sub $0,1
+  mul $2,9
+  add $2,$3
+  add $3,$2
+  cmp $2,8
+lpe
+mov $0,$3
---
+++ oeis/229/A229062.asm
@@ -0,0 +1,36 @@
+; A229062: 1 if n is representable as sum of two nonnegative squares, otherwise 0.
+; Submitted by Jon Maiga
+; 1,1,1,0,1,1,0,0,1,1,1,0,0,1,0,0,1,1,1,0,1,0,0,0,0,1,1,0,0,1,0,0,1,0,1,0,1,1,0,0,1,1,0,0,0,1,0,0,0,1,1,0,1,1,0,0,0,0,1,0,0,1,0,0,1,1,0,0,1,0,0,0,1,1,1,0,0,0,0,0,1,1,1,0,0,1,0,0,0,1,1,0,0,0,0,0,0,1,1,0
+
+mov $1,1
+lpb $0
+  mov $3,$0
+  lpb $3
+    mov $4,$0
+    mov $6,$2
+    cmp $6,0
+    add $2,$6
+    mod $4,$2
+    cmp $4,0
+    cmp $4,0
+    mov $5,$2
+    add $2,1
+    cmp $5,1
+    max $4,$5
+    sub $3,$4
+  lpe
+  mov $5,1
+  lpb $0
+    mov $6,$2
+    cmp $6,0
+    add $2,$6
+    dif $0,$2
+    mul $5,$2
+  lpe
+  lpb $1
+    add $5,1
+    mul $1,$5
+    mod $1,4
+  lpe
+lpe
+mov $0,$1
