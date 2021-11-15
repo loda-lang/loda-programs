203352e6cf6350cd5beebaf9cfe779211569c030
Christian Krause <me@ckrause.org>
2021-11-15 18:34:39 +0100

    updated 35 programs

--- oeis/107/A107757.asm
+++ oeis/107/A107757.asm
@@ -1,11 +1,17 @@
 ; A107757: Numbers k such that Sum_{j=1..k} Catalan(j) == 2 (mod 3).
-; Submitted by Jamie Morken(s3)
+; Submitted by Jamie Morken(s1)
 ; 3,9,11,27,29,35,39,81,83,89,93,107,111,117,119,243,245,251,255,269,273,279,281,323,327,333,335,351,353,359,363,729,731,737,741,755,759,765,767,809,813,819,821,837,839,845,849,971,975,981,983,999,1001,1007,1011
 
 add $0,1
-seq $0,5836 ; Numbers n whose base 3 representation contains no 2.
-sub $0,1
-mul $0,3
-div $0,2
+mov $2,$0
+div $0,4
+seq $0,240400 ; Numbers n having a partition into distinct parts of form 3^k-2^k.
 mul $0,2
-add $0,3
+add $0,$2
+mul $0,9
+div $0,6
+mul $0,6
+add $0,2
+div $0,4
+mul $0,2
+sub $0,1
--- oeis/158/A158746.asm
+++ oeis/158/A158746.asm
@@ -11,13 +11,12 @@ lpb $2
   mov $3,$1
   seq $3,10051 ; Characteristic function of primes: 1 if n is prime, else 0.
   sub $0,$3
-  add $1,31
+  add $1,30
   mov $4,$0
   max $4,0
   cmp $4,$0
-  sub $1,$4
   mul $2,$4
 lpe
 mov $0,$1
-sub $0,43
+sub $0,42
 div $0,30
