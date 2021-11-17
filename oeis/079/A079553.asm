66a96e576e81b40c0e35629fc3e092e5737a25fb
Simon Strandgaard <neoneye@gmail.com>
2021-11-17 21:39:31 +0100

    Fixed incorrect program, that was using indirect addressing and only had its 100 first terms verified. However it diverged from the b-file. I have validated the new program up to 750 terms, where it starts to get slow.

--- oeis/079/A079553.asm
+++ oeis/079/A079553.asm
@@ -1,5 +1,11 @@
 ; A079553: a(n) = floor( d(n^2) / d(n) ), where d() = A000005.
+; Submitted by Simon Strandgaard
 ; 1,1,1,1,1,2,1,1,1,2,1,2,1,2,2,1,1,2,1,2,2,2,1,2,1,2,1,2,1,3,1,1,2,2,2,2,1,2,2,2,1,3,1,2,2,2,1,2,1,2,2,2,1,2,2,2,2,2,1,3,1,2,2,1,2,3,1,2,2,3,1,2,1,2,2,2,2,3,1,2,1,2,1,3,2,2,2,2,1,3,2,2,2,2,2,2,1,2,2,2
 
-mov $$0,1
-seq $0,322307 ; Number of multisets in the swell of the n-th multiset multisystem.
+mov $1,$0
+seq $1,5 ; d(n) (also called tau(n) or sigma_0(n)), the number of divisors of n.
+add $0,1
+pow $0,2
+trn $0,1
+seq $0,5 ; d(n) (also called tau(n) or sigma_0(n)), the number of divisors of n.
+div $0,$1
