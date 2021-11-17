288f9f16acb3a22b8f11f69b504a5438b0d0d4a0
Simon Strandgaard <neoneye@gmail.com>
2021-11-17 21:27:54 +0100

    Fixed incorrect program, that was using indirect addressing and only had its 100 first terms verified. However it diverged around the 105 terms. The new program have been validated up to 5k terms.

--- oeis/065/A065803.asm
+++ oeis/065/A065803.asm
@@ -1,7 +1,9 @@
 ; A065803: a(n) = (sigma_2(n) mod 2) * (sigma_2(n) mod 5). Residue-product modulo 2 and 5 of sum of square of divisors.
-; Submitted by Jon Maiga
+; Submitted by Simon Strandgaard
 ; 1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1
 
-seq $0,7913 ; Squarefree part of n: a(n) is the smallest positive number m such that n/m is a square.
-mov $$0,$0
-mov $0,$1
+seq $0,1157 ; sigma_2(n): sum of squares of divisors of n.
+mov $1,$0
+mod $0,2
+mod $1,5
+mul $0,$1
