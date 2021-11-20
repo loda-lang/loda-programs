7649edd5df7624813f4184fc420a02403b39ad75
Christian Krause <me@ckrause.org>
2021-11-20 18:22:45 +0100

    added 52 programs

---
+++ oeis/247/A247684.asm
@@ -0,0 +1,30 @@
+; A247684: Decimal expansion of the integral over the first quadrant (x>0, y>0) of sqrt(x^2 + x*y + y^2)*exp(-x-y) dx dy.
+; Submitted by Jamie Morken(s1)
+; 1,8,2,3,9,5,9,2,1,6,5,0,1,0,8,2,2,6,8,5,4,6,4,3,3,9,2,7,6,9,1,8,9,4,2,7,8,4,8,5,6,1,7,9,1,8,3,6,7,0,6,2,0,8,8,8,0,1,0,2,0,7,5,0,2,2,8,1,2,0,7,1,9,9,1,3,9,5,6,7,2,5,1,5,5,2,1,1,8,1,6,1,1,0,2,9,9,0,6,6
+
+mov $2,2
+mov $3,$0
+mul $3,3
+lpb $3
+  mul $1,2
+  mul $1,$3
+  mul $2,-3
+  mov $5,$3
+  mul $5,2
+  add $5,1
+  mul $2,$5
+  add $1,$2
+  cmp $4,0
+  mov $5,$0
+  add $5,$4
+  div $1,$5
+  div $2,$5
+  sub $3,1
+lpe
+mov $6,10
+pow $6,$0
+div $2,$6
+div $1,$2
+add $1,$6
+mov $0,$1
+mod $0,10
---
+++ oeis/327/A327555.asm
@@ -0,0 +1,28 @@
+; A327555: Decimal expansion of number with continued fraction expansion [1; 2, 1, 4, 1, 6, 1, 8, 1, 10, 1, ...].
+; Submitted by Jamie Morken(s2)
+; 1,3,5,3,3,8,9,2,3,4,1,1,7,6,7,5,8,4,1,9,3,5,7,0,8,9,0,5,7,6,0,4,8,7,2,7,2,2,0,0,5,0,7,5,8,4,3,3,1,5,8,4,3,6,9,4,1,2,5,2,5,4,5,6,6,1,2,2,3,8,4,2,2,7,4,1,8,0
+
+mov $1,1
+mov $2,1
+mov $3,$0
+mul $3,4
+lpb $3
+  mul $1,$3
+  add $2,$1
+  mul $2,$3
+  add $1,$2
+  sub $3,1
+  cmp $6,0
+  add $5,$6
+  div $1,$5
+  div $2,$5
+  add $2,$1
+  add $2,$1
+  sub $3,1
+lpe
+mov $4,10
+pow $4,$0
+div $2,$4
+div $1,$2
+mod $1,10
+mov $0,$1
