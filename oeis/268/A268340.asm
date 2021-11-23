f3a13424625301f7570580e7968766ea45722152
Christian Krause <me@ckrause.org>
2021-11-23 18:13:36 +0100

    updated 114 programs

--- oeis/268/A268340.asm
+++ oeis/268/A268340.asm
@@ -1,21 +1,22 @@
 ; A268340: Characteristic function of the prime powers p^k, k >= 2.
-; Submitted by Jon Maiga
+; Submitted by Christian Krause
 ; 0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
-seq $0,100994 ; If n is a prime power p^m, m >= 1, then n, otherwise 1.
-sub $0,1
-seq $0,5 ; d(n) (also called tau(n) or sigma_0(n)), the number of divisors of n.
-sub $0,1
-mul $0,2
-add $0,2
-mov $3,5
-lpb $3
+lpb $0
+  seq $0,14963 ; Exponential of Mangoldt function M(n): a(n) = 1 unless n is a prime or prime power when a(n) = that prime.
+  mov $1,$0
+  sub $0,1
+lpe
+lpb $0
+  sub $0,1
+  add $2,$1
+lpe
+pow $1,$0
+mov $0,$1
+lpb $1
   lpb $0
-    sub $0,3
-    add $2,$3
-    div $3,$2
+    add $2,1
+    sub $0,$2
+    cmp $1,0
   lpe
 lpe
-mov $0,$3
-add $0,1
-mod $0,2
