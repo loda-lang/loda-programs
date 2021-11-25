33e08bc4738d5e1c3406a29e889472d15396538a
Simon Strandgaard <neoneye@gmail.com>
2021-11-25 18:35:31 +0100

    First version

---
+++ oeis/256/A256307.asm
@@ -0,0 +1,17 @@
+; A256307: Apply the transformation 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 0 to the digits of n written in base 7; do not convert back to base 10.
+; Submitted by Simon Strandgaard
+; 1,2,3,4,5,6,0,21,22,23,24,25,26,20,31,32,33,34,35,36,30,41,42,43,44,45,46,40,51,52,53,54,55,56,50,61,62,63,64,65
+
+bin $2,$0
+mov $3,1
+lpb $0
+  mov $2,$0
+  div $0,7
+  add $2,8
+  mod $2,7
+  mul $2,$3
+  add $1,$2
+  mov $2,$1
+  mul $3,10
+lpe
+mov $0,$2
