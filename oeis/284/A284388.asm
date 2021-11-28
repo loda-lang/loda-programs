6ac07a84ddf2fa3f130181fb8403d343e0a8d0a4
Christian Krause <me@ckrause.org>
2021-11-28 16:06:14 +0100

    added 79 programs

---
+++ oeis/284/A284388.asm
@@ -0,0 +1,14 @@
+; A284388: 0-limiting word of the morphism 0 -> 1, 1 -> 001.
+; Submitted by Christian Krause
+; 0,0,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,0,0,1,0
+
+lpb $0
+  sub $0,1
+  mov $1,$0
+  div $0,50405
+  trn $0,9
+  max $1,0
+  seq $1,26465 ; Length of n-th run of identical symbols in the Thue-Morse sequence A010060 (or A001285).
+lpe
+div $1,2
+mov $0,$1
