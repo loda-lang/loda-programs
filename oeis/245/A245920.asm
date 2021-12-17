2fe4de2ec16d565d69d3e4508a0af75f5be6f1ab
Christian Krause <me@ckrause.org>
2021-12-17 16:45:43 +0100

    deleted 4 programs

--- oeis/245/A245920.asm
+++
@@ -1,5 +0,0 @@
-; A245920: Limit-reverse of the (2,1)-version of the infinite Fibonacci word A014675 with first term as initial block.
-; 2,1,2,1,2,2,1,2,1,2,2,1,2,2,1,2,1,2,2,1,2,2,1,2,1,2,2,1,2,1,2,2,1,2,2,1,2,1,2,2,1,2,1,2,2,1,2,2,1,2,1,2,2,1,2,2,1,2,1,2,2,1,2,1,2,2,1,2,2,1,2,1,2,2,1,2,2,1,2,1,2,2,1,2,1,2,2,1,2,2,1,2,1,2,2,1,2,1,2,2
-
-add $0,100
-seq $0,14675 ; The infinite Fibonacci word (start with 1, apply 1->2, 2->21, take limit).
