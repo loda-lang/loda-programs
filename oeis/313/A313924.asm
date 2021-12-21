a0b6913e85960231af0b9d69d47ddc8cf1388240
Christian Krause <me@ckrause.org>
2021-12-21 10:35:57 +0100

    added 40 programs

---
+++ oeis/313/A313924.asm
@@ -0,0 +1,13 @@
+; A313924: Coordination sequence Gal.6.626.6 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+; Submitted by Christian Krause
+; 1,5,10,16,22,28,33,38,44,50,56,61,66,71,76,82,88,94,99,104,110,116,122,127,132,137,142,148,154,160,165,170,176,182,188,193,198,203,208,214,220,226,231,236,242,248,254,259,264,269
+
+mov $1,$0
+seq $1,313485 ; Coordination sequence Gal.6.254.6 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+mov $2,$0
+mul $0,6
+sub $0,1
+mod $0,$1
+add $0,1
+mul $2,4
+add $0,$2
---
+++ oeis/315/A315499.asm
@@ -0,0 +1,14 @@
+; A315499: Coordination sequence Gal.6.643.3 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+; Submitted by Christian Krause
+; 1,6,11,16,22,28,34,40,46,52,57,62,68,74,79,84,90,96,102,108,114,120,125,130,136,142,147,152,158,164,170,176,182,188,193,198,204,210,215,220,226,232,238,244,250,256,261,266,272,278
+
+mov $1,$0
+seq $1,313023 ; Coordination sequence Gal.6.130.6 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+mov $2,$0
+mul $0,6
+sub $0,1
+mod $0,$1
+add $0,29
+mul $2,4
+add $0,$2
+sub $0,28
