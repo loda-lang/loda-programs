f3b36dc21fb90c12d120a34fb9ad5b0659905a82
Christian Krause <me@ckrause.org>
2021-12-21 10:38:20 +0100

    updated 50 programs

--- oeis/123/A123740.asm
+++ oeis/123/A123740.asm
@@ -1,6 +1,15 @@
 ; A123740: Characteristic sequence for Wythoff AB-numbers A003623.
-; Submitted by Jamie Morken(s1)
+; Submitted by Jon Maiga
 ; 0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1
 
-seq $0,159917 ; Fixed point of the morphism 0 -> 01, 1 -> 2, 2 -> 01, starting from a(0) = 0.
-div $0,2
+seq $0,139764 ; Smallest term in Zeckendorf representation of n.
+pow $0,2
+mov $1,2
+lpb $0
+  mul $0,2
+  div $0,5
+  mul $1,2
+lpe
+mov $0,$1
+div $0,12
+mod $0,2
--- oeis/280/A280521.asm
+++ oeis/280/A280521.asm
@@ -1,16 +1,15 @@
 ; A280521: From the "Fibonachos" game: Number of phases of the following routine: during each phase, the player removes objects from a pile of n objects as the Fibonacci sequence until the pile contains fewer objects than the next Fibonacci number. The phases continue until the pile is empty.
-; Submitted by Jon Maiga
+; Submitted by Jamie Morken(s2)
 ; 1,1,2,1,2,2,1,2,2,3,2,1,2,2,3,2,3,3,2,1,2,2,3,2,3,3,2,3,3,4,3,2,1,2,2,3,2,3,3,2,3,3,4,3,2,3,3,4,3,4,4,3,2,1,2,2,3,2,3,3,2,3,3,4,3,2,3,3,4,3,4,4,3,2,3,3,4,3,4,4,3,4,4,5,4,3,2,1,2,2,3,2,3,3,2,3,3,4,3,2
 
-add $0,1
-mov $1,4
 lpb $0
   mov $2,$0
-  add $2,1
+  add $2,2
   seq $2,66628 ; a(n) = n - the largest Fibonacci number <= n.
   mov $0,$2
+  sub $0,1
   add $1,2
 lpe
 div $1,2
 mov $0,$1
-sub $0,2
+add $0,1
