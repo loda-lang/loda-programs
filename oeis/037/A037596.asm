298fb39677c645439ad1633392f2bcf522876151
Christian Krause <me@ckrause.org>
2021-12-24 17:44:43 +0100

    updated 45 programs

--- oeis/037/A037596.asm
+++ oeis/037/A037596.asm
@@ -1,7 +1,15 @@
 ; A037596: Numbers written in base 4 whose digits are, in order, the first n terms of the periodic sequence with initial period 1,0,3.
-; Submitted by Jamie Morken(s2)
+; Submitted by Jamie Morken(s3)
 ; 1,10,103,1031,10310,103103,1031031,10310310,103103103,1031031031,10310310310,103103103103,1031031031031,10310310310310,103103103103103,1031031031031031
 
-seq $0,37554 ; Base 5 digits are, in order, the first n terms of the periodic sequence with initial period 2,1,1.
-div $0,2
-seq $0,7091 ; Numbers in base 5.
+mov $2,1
+lpb $0
+  sub $0,1
+  add $1,$2
+  mul $1,10
+  add $2,23
+  div $2,2
+  mod $2,4
+lpe
+add $1,$2
+mov $0,$1
--- oeis/037/A037657.asm
+++ oeis/037/A037657.asm
@@ -1,16 +1,14 @@
 ; A037657: Base 8 digits are, in order, the first n terms of the periodic sequence with initial period 3,0,2.
-; Submitted by Jamie Morken(s4)
+; Submitted by Jamie Morken(s3)
 ; 3,24,194,1555,12440,99522,796179,6369432,50955458,407643667,3261149336,26089194690,208713557523,1669708460184,13357667681474,106861341451795,854890731614360,6839125852914882
 
 mov $2,3
 lpb $0
   sub $0,1
-  sub $2,1
   add $1,$2
-  add $1,1
   mul $1,8
-  add $2,2
-  max $2,2
+  add $2,21
+  div $2,2
   mod $2,4
 lpe
 add $1,$2
