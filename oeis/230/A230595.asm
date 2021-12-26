4958b0ee704a0e632ab9c13df1046fe86cb22554
Christian Krause <me@ckrause.org>
2021-12-26 11:50:30 +0100

    added 197 programs

---
+++ oeis/230/A230595.asm
@@ -0,0 +1,10 @@
+; A230595: Number of ways to write n as n = x*y, where x and y are primes, 1 <= x <= n, 1 <= y <= n.
+; Submitted by Christian Krause
+; 0,0,0,1,0,2,0,0,1,2,0,0,0,2,2,0,0,0,0,0,2,2,0,0,1,2,0,0,0,0,0,0,2,2,2,0,0,2,2,0,0,0,0,0,0,2,0,0,1,0,2,0,0,0,2,0,2,2,0,0,0,2,0,0,2,0,0,0,2,0,0,0,0,2,0,0,2,0,0,0,0,2,0,0,2,2,2,0,0,0,2,0,2,2,2,0,0,0,0,0
+
+mov $1,$0
+seq $0,1221 ; Number of distinct primes dividing n (also called omega(n)).
+seq $1,86436 ; Maximum number of parts possible in a factorization of n; a(1) = 1, and for n > 1, a(n) = A001222(n) = bigomega(n).
+sub $1,1
+cmp $1,1
+mul $0,$1
---
+++ oeis/345/A345132.asm
@@ -0,0 +1,19 @@
+; A345132: Number of (n+2) X (n+2) symmetric matrices with nonnegative integer entries, trace 0, with n rows that sum to 2, and 2 rows that sum to 1.
+; Submitted by Christian Krause
+; 1,1,3,10,46,252,1642,12316,104730,995122,10450414,120192924,1502537932,20285580880,294156077364,4559608340968,75236088623548,1316668510772124,24358939966126900,475008770990906488,9737844963832507656,209366721066736679536
+
+mov $2,1
+mov $4,1
+lpb $0
+  sub $0,1
+  add $1,$4
+  mul $1,$0
+  mov $3,$2
+  add $2,$1
+  mul $4,$0
+  add $4,$1
+  div $4,2
+  add $2,$4
+  mov $4,$3
+lpe
+mov $0,$2
