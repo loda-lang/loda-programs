9450184377fae4644cea6ba9f3239bca733302e7
Christian Krause <me@ckrause.org>
2021-12-27 21:44:11 +0100

    added 133 programs

---
+++ oeis/103/A103659.asm
@@ -0,0 +1,21 @@
+; A103659: (1/6) * most frequently occurring volume assumed by triangular pyramids with their 4 vertices chosen from distinct points of an (n+1)X(n+1)X(n+1) lattice cube.
+; Submitted by Christian Krause
+; 1,2,2,4,4,12,12,12,12,12,24,24,24,24
+
+mul $0,2
+mov $1,1
+mov $2,1
+mov $3,1
+lpb $0
+  sub $0,2
+  max $0,$2
+  sub $1,$3
+  mul $1,2
+  add $2,$3
+  add $3,$1
+  add $1,$2
+  div $1,2
+  add $1,$2
+  mul $3,2
+lpe
+mov $0,$2
