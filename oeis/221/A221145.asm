8c5a15e56e0b80b95edf8fe3c41e0262916848d1
Christian Krause <me@ckrause.org>
2021-12-27 11:38:06 +0100

    added 182 programs

---
+++ oeis/221/A221145.asm
@@ -0,0 +1,9 @@
+; A221145: a(n) is the number of permutations of n elements with exactly one fixed point and no transpositions.
+; Submitted by Christian Krause
+; 1,0,0,8,30,144,1120,9120,80892,808640,8908416,106889760,1389405160,19452003648,291782125440,4668506495744,79364583206160,1428562661736960,27142690917071872,542853814698737280,11399930105272855776,250798462399704792320
+
+mov $1,$0
+add $0,1
+seq $1,38205 ; Number of derangements of n where minimal cycle size is at least 3.
+mul $1,$0
+mov $0,$1
---
+++ oeis/338/A338939.asm
@@ -0,0 +1,9 @@
+; A338939: a(n) is the number of partitions n = a + b such that a*b is a perfect square.
+; Submitted by Christian Krause
+; 0,1,0,1,1,1,0,1,0,3,0,1,1,1,1,1,1,1,0,3,0,1,0,1,2,3,0,1,1,3,0,1,0,3,1,1,1,1,1,3,1,1,0,1,1,1,0,1,0,5,1,3,1,1,1,1,0,3,0,3,1,1,0,1,4,1,0,3,0,3,0,1,1,3,2,1,0,3,0,3,0,3,0,1,4,1,1,1,1,3,1,1,0,1,1,1,1,1,0,5
+
+mov $1,$0
+seq $0,256452 ; Number of integer solutions to n^2 = x^2 + y^2 with x>0, y>=0.
+mov $2,$1
+gcd $2,2
+div $0,$2
