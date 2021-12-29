517da19e6507e1edb917d04ea4d7e15d1d480364
Christian Krause <me@ckrause.org>
2021-12-29 19:38:09 +0100

    added 193 programs

---
+++ oeis/192/A192379.asm
@@ -0,0 +1,22 @@
+; A192379: Constant term of the reduction by x^2->x+1 of the polynomial p(n,x) defined below in Comments.
+; Submitted by Christian Krause
+; 1,0,5,8,45,128,505,1680,6089,21120,74909,262680,926485,3258112,11474865,40382752,142171985,500432640,1761656821,6201182760,21829269181,76841888640,270495370025,952182350768,3351823875225,11798909226368
+
+mov $1,1
+mov $4,-1
+lpb $0
+  sub $0,1
+  add $2,$1
+  add $4,$3
+  add $3,$4
+  add $1,$3
+  add $4,$2
+  add $3,$4
+  sub $4,$3
+  sub $2,$4
+  add $3,1
+  add $3,$4
+  add $3,$2
+  add $3,$4
+lpe
+mov $0,$1
---
+++ oeis/261/A261664.asm
@@ -0,0 +1,19 @@
+; A261664: Number of equivalence classes of permutations avoiding the pattern {231}.
+; Submitted by Christian Krause
+; 1,1,2,4,9,21,50,121,296,729
+
+lpb $0
+  sub $0,1
+  add $4,$1
+  add $1,$3
+  div $1,2
+  add $5,$2
+  mov $2,$3
+  mov $3,$5
+  max $4,1
+  mul $4,2
+  add $2,$4
+lpe
+mov $0,$3
+div $0,2
+add $0,1
