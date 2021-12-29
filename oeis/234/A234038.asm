a54b48c8ddc4b1fc2fe8cdef602464285ddc9da2
Christian Krause <me@ckrause.org>
2021-12-29 20:28:47 +0100

    updated 11 programs

--- oeis/234/A234038.asm
+++ oeis/234/A234038.asm
@@ -1,20 +1,8 @@
 ; A234038: Smallest positive integer solution x of 9*x - 2^n*y = 1.
-; Submitted by Jamie Morken(s4)
+; Submitted by Jamie Morken(s2)
 ; 1,1,1,1,9,25,57,57,57,57,569,1593,3641,3641,3641,3641,36409,101945,233017,233017,233017,233017,2330169,6524473,14913081,14913081,14913081,14913081,149130809,417566265,954437177,954437177,954437177
 
-mov $1,1
-mov $3,1
-lpb $0
-  sub $0,1
-  trn $2,$3
-  add $2,$1
-  mul $2,2
-  mov $4,$0
-  add $4,$2
-  add $4,$1
-  add $4,1
-  add $1,$4
-  mul $3,2
-  mod $1,$3
-lpe
-mov $0,$1
+seq $0,153234 ; a(n) = floor(2^n/9).
+seq $0,30101 ; a(n) is the number produced when n is converted to binary digits, the binary digits are reversed and then converted back into a decimal number.
+mul $0,8
+add $0,1
