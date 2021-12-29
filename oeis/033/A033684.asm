2a58f719dc1fe9d08fc7c29cc72d4d7471abebad
Christian Krause <me@ckrause.org>
2021-12-29 20:27:36 +0100

    deleted 1 programs

--- oeis/033/A033684.asm
+++
@@ -1,7 +0,0 @@
-; A033684: 1 iff n is a square not divisible by 3.
-; Submitted by Jamie Morken(s2)
-; 0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-
-seq $0,282213 ; Coefficients in q-expansion of (E_2^3*E_4 - 3*E_2^2*E_6 + 3*E_2*E_4^2 - E_4*E_6)/3456, where E_2, E_4, E_6 are the Eisenstein series shown in A006352, A004009, A013973, respectively.
-mod $0,9
-mod $0,2
