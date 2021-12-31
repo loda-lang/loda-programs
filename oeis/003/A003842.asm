90b5eaa70bc09a5065dff5c84fe3b62b6b4efdce
Christian Krause <me@ckrause.org>
2021-12-31 17:12:21 +0100

    deleted 2 programs

--- oeis/003/A003842.asm
+++
@@ -1,6 +0,0 @@
-; A003842: The infinite Fibonacci word: start with 1, repeatedly apply the morphism 1->12, 2->1, take limit; or, start with S(0)=2, S(1)=1, and for n>1 define S(n)=S(n-1)S(n-2), then the sequence is S(oo).
-; Submitted by Jon Maiga
-; 1,2,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,2,1,2,1,1,2,1
-
-seq $0,3849 ; The infinite Fibonacci word (start with 0, apply 0->01, 1->0, take limit).
-add $0,1
