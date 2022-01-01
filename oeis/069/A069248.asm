de4bc4e22d174e340b74b0563903e13a193abb6c
Christian Krause <me@ckrause.org>
2022-01-01 19:28:24 +0100

    added 107 programs

---
+++ oeis/069/A069248.asm
@@ -0,0 +1,32 @@
+; A069248: Number of positive divisors of n themselves divisible by largest prime that divides n.
+; Submitted by Jon Maiga
+; 0,1,1,2,1,2,1,3,2,2,1,3,1,2,2,4,1,4,1,3,2,2,1,4,2,2,3,3,1,4,1,5,2,2,2,6,1,2,2,4,1,4,1,3,3,2,1,5,2,4,2,3,1,6,2,4,2,2,1,6,1,2,3,6,2,4,1,3,2,4,1,8,1,2,4,3,2,4,1,5,4,2,1,6,2,2,2,4,1,6,2,3,2,2,2,6,1,4,3,6
+
+add $0,1
+mov $1,1
+lpb $0
+  mov $3,$0
+  add $7,$1
+  lpb $3
+    mov $1,$7
+    mov $4,$0
+    mov $6,$2
+    cmp $6,0
+    add $2,$6
+    mod $4,$2
+    cmp $4,0
+    cmp $4,0
+    mov $5,$2
+    add $2,1
+    cmp $5,1
+    max $4,$5
+    sub $3,$4
+  lpe
+  mov $5,1
+  lpb $0
+    dif $0,$2
+    mul $0,$5
+    mul $5,$2
+  lpe
+lpe
+mov $0,$7
---
+++ oeis/121/A121373.asm
@@ -0,0 +1,7 @@
+; A121373: Expansion of f(x) = f(x, -x^2) in powers of x where f(, ) is Ramanujan's general theta function.
+; Submitted by Jon Maiga
+; 1,1,-1,0,0,-1,0,-1,0,0,0,0,-1,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0
+
+seq $0,133079 ; Expansion of f(x)^3 - 3 * x * f(x^9)^3 in powers of x^3 where f() is a Ramanujan theta function.
+mod $0,3
+dif $0,-2
