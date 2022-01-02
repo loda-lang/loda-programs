b42e9e5059f7e5a293b9680edd61be09b2a5ae5b
Christian Krause <me@ckrause.org>
2022-01-02 13:38:11 +0100

    added 8 programs

---
+++ oeis/300/A300070.asm
@@ -0,0 +1,28 @@
+; A300070: Decimal expansion of the positive member y of a triple (x, y, z) solving a certain historical system of three equations.
+; Submitted by Christian Krause
+; 3,2,6,9,9,2,8,3,0,3,8,2,0,8,7,0,5,8,0,2,3,9,1,7,8,1,3,6,8,5,9,2,6,6,8,6,9,9,7,6,4,9,4,3,1,0,1,7,1,6,6,6,9,3,2,4,0,5,9,5,8,7,9,9,1,7,0,1,8,5,5,6,3,5,8,5,8,2,7,8,1,0,6,1,5,8,8,5,0,5,3,9,9,5,3,4,5,6,0,5
+
+add $0,1
+mov $2,2
+mov $3,$0
+mul $3,4
+lpb $3
+  add $6,$2
+  add $1,$6
+  mov $7,$2
+  add $2,$1
+  sub $3,1
+  add $5,$2
+  add $6,$5
+lpe
+mov $2,$1
+mov $4,10
+pow $4,$0
+add $5,$7
+mov $1,$5
+add $2,$5
+div $2,$4
+div $1,$2
+mov $0,$1
+div $0,2
+mod $0,10
