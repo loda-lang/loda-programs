e486e57dfb23b400b1c4532b3551d166266512fa
Christian Krause <me@ckrause.org>
2022-01-03 18:05:50 +0100

    updated 1 programs

--- oeis/005/A005882.asm
+++ oeis/005/A005882.asm
@@ -1,8 +1,7 @@
 ; A005882: Theta series of planar hexagonal lattice (A2) with respect to deep hole.
-; Submitted by Christian Krause
+; Submitted by Simon Strandgaard
 ; 3,3,6,0,6,3,6,0,3,6,6,0,6,0,6,0,9,6,0,0,6,3,6,0,6,6,6,0,0,0,12,0,6,3,6,0,6,6,0,0,3,6,6,0,12,0,6,0,0,6,6,0,6,0,6,0,9,6,6,0,6,0,0,0,6,9,6,0,0,6,6,0,12,0,6,0,6,0,0,0,6,6,12,0,0,3,12,0,0,6,6,0,6,0,6,0,3,6,0,0
 
-mov $1,$0
-seq $1,33687 ; Theta series of hexagonal lattice A_2 with respect to deep hole divided by 3.
-mov $0,$1
+mul $0,3
+seq $0,35178 ; a(n) = Sum_{d|n} Kronecker(-12, d) (= A134667(d)).
 mul $0,3
