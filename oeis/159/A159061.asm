d48653bbbf3f17bf900ec3ecca4d03486e1286db
Christian Krause <me@ckrause.org>
2022-01-04 18:41:49 +0000

    added 152 programs

---
+++ oeis/159/A159061.asm
@@ -0,0 +1,15 @@
+; A159061: Nearest integer to the expected number of tosses of a fair coin required to obtain at least n heads and n tails.
+; Submitted by Christian Krause
+; 3,6,8,10,12,15,17,19,21,24,26,28,30,32,34,36,39,41,43,45,47,49,51,53,56,58,60,62,64,66,68,70,72,75,77,79,81,83,85,87,89,91,93,95,98,100,102,104,106,108,110,112,114,116,118,120,123,125,127,129,131,133,135,137
+
+mov $2,$0
+mul $0,2
+seq $0,338720 ; Define b(1)=1 and for n>1, b(n)=n/b(n-1); then a(n) = nearest integer to b(n).
+mul $0,2
+mov $3,$2
+mul $3,2
+add $0,$3
+add $0,$2
+add $0,$2
+div $0,2
+add $0,2
---
+++ oeis/168/A168258.asm
@@ -0,0 +1,14 @@
+; A168258: Triangle read by rows, A097806 * A111967 as infinite lower triangular matrices.
+; Submitted by Christian Krause
+; 1,1,1,2,2,1,2,2,2,1,3,3,3,2,1,3,3,3,3,2,1,4,4,4,4,3,2,1,4,4,4,4,4,3,2,1,5,5,5,5,5,4,3,2,1,5,5,5,5,5,5,4,3,2,1,6,6,6,6,6,6,5,4,3,2,1,6,6,6,6,6,6,6,5,4,3,2,1
+
+lpb $0
+  add $1,1
+  sub $0,$1
+  mov $2,$1
+  sub $2,$0
+lpe
+min $0,$2
+add $0,$2
+add $0,2
+div $0,2
