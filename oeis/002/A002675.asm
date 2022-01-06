da88e980139b3d0bfcc4230a538501024088cc22
Christian Krause <me@ckrause.org>
2022-01-06 18:18:31 +0000

    added 94 programs

---
+++ oeis/002/A002675.asm
@@ -0,0 +1,9 @@
+; A002675: Numerators of coefficients for central differences M_{4}^(2*n).
+; Submitted by Christian Krause
+; 1,1,1,17,31,1,5461,257,73,1271,60787,241,22369621,617093,49981,16843009,5726623061,7957,91625968981,61681,231927781,50991843607,499069107643,4043309297,1100586419201,5664905191661,1672180312771
+
+mul $0,2
+mov $1,$0
+add $1,1
+seq $1,2678 ; Numerators of the Taylor coefficients of (e^x-1)^2.
+mov $0,$1
---
+++ oeis/120/A120787.asm
@@ -0,0 +1,19 @@
+; A120787: Denominators of partial sums of Catalan numbers scaled by powers of 1/20.
+; Submitted by Christian Krause
+; 1,20,200,1600,80000,1600000,16000000,1280000000,2560000000,256000000000,2560000000000,102400000000000,1024000000000000,4096000000000000,40960000000000000,6553600000000000000
+
+mul $0,2
+mov $1,1
+lpb $0
+  mov $2,$0
+  sub $0,1
+  add $3,$1
+  mul $3,$0
+  sub $0,1
+  mul $1,5
+  add $2,2
+  mul $1,$2
+lpe
+gcd $3,$1
+div $1,$3
+mov $0,$1
