14ed59749b916549fbdf340ab403f238fe823774
Christian Krause <me@ckrause.org>
2022-01-07 17:03:31 +0000

    updated 19 programs

--- oeis/055/A055995.asm
+++ oeis/055/A055995.asm
@@ -5,7 +5,6 @@
 mov $4,2
 mov $7,$0
 lpb $4
-  sub $4,1
   mov $9,2
   mov $11,$7
   lpb $9
@@ -14,24 +13,25 @@ lpb $4
     add $0,$9
     sub $0,1
     mov $2,$0
-    mov $13,2
-    mov $14,0
-    lpb $13
+    mov $3,2
+    mov $5,0
+    lpb $3
       mov $0,$2
-      sub $13,1
-      add $0,$13
+      sub $3,1
+      add $0,$3
       sub $0,1
       mov $6,9
       pow $6,$0
       mov $0,$6
-      mov $12,$13
+      mov $12,$3
       mul $12,$6
-      add $14,$12
+      add $5,$12
     lpe
     min $2,1
     mul $2,$0
-    mov $0,$14
+    mov $0,$5
     sub $0,$2
+    mov $4,1
     mov $8,$9
     mul $8,$0
     add $10,$8
@@ -40,8 +40,4 @@ lpb $4
   mul $11,$0
   mov $0,$10
   sub $0,$11
-  mov $3,$4
-  mul $3,$0
-  add $5,$3
 lpe
-mov $0,$5
--- oeis/063/A063244.asm
+++ oeis/063/A063244.asm
@@ -1,21 +1,15 @@
 ; A063244: Dimension of the space of weight 2n cuspidal newforms for Gamma_0( 94 ).
+; Submitted by Christian Krause
 ; 3,12,20,26,34,44,48,58,66,72,80,90,94,104,112,118,126,136,140,150,158,164,172,182,186,196,204,210,218,228,232,242,250,256,264,274,278,288,296,302,310,320,324,334,342,348,356,366,370,380
 
+mov $1,$0
+mul $0,9
+div $1,3
+mul $1,4
 mov $2,$0
-mov $3,$0
-mul $0,2
-mov $5,1
-lpb $0
-  sub $0,1
-  trn $0,2
-  add $3,5
-  div $3,2
-  mul $3,2
-  mov $5,$3
-lpe
-mov $1,$5
-add $1,2
-mov $4,$2
-mul $4,4
-add $1,$4
-mov $0,$1
+trn $0,3
+mod $0,2
+add $0,$1
+sub $2,$0
+mov $0,$2
+add $0,3
