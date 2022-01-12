ddfdac4de0376ab3fb364307babbf010a44827e1
Christian Krause <me@ckrause.org>
2022-01-12 18:38:06 +0000

    added 98 programs

---
+++ oeis/130/A130719.asm
@@ -0,0 +1,21 @@
+; A130719: a(n) = n-th digit after the decimal point of the decimal representation of the n-th harmonic number.
+; Submitted by Jamie Morken(w2)
+; 0,0,3,3,3,0,1,4,3,9,7,0,7,2,3,2,3,0,4,8,1,8,4,3,6,1,1,6,9,4,4,0,9,1,3,7,1,4,2,6,0,6,0,2,3,5,2,9,4,5,3,6,7,8,4,3,5,3,3,5,0,5,2,1,0,2,0,8,8,7,0,0,1,7,9,9,8,5,1,0
+
+add $0,1
+mov $2,1
+mov $3,$0
+mov $0,1
+lpb $3
+  mul $0,10
+  mul $1,$3
+  mov $4,$3
+  add $4,1
+  mul $2,$4
+  add $1,$2
+  sub $3,1
+lpe
+mul $1,$0
+div $1,$2
+mov $0,$1
+mod $0,10
---
+++ oeis/227/A227133.asm
@@ -0,0 +1,32 @@
+; A227133: Given a square grid with side n consisting of n^2 cells (or points), a(n) is the maximum number of points that can be painted so that no four of the painted ones form a square with sides parallel to the grid.
+; Submitted by Jamie Morken(w3)
+; 1,3,7,12,17,24,32,41,51,61,73,85,98
+
+mov $4,$0
+add $4,1
+mov $8,$0
+lpb $4
+  mov $0,$8
+  mov $1,0
+  mov $2,0
+  sub $4,1
+  sub $0,$4
+  add $0,1
+  mov $3,$0
+  mul $3,4
+  sub $3,1
+  mov $5,1
+  mov $6,0
+  lpb $3
+    add $6,$5
+    add $1,$6
+    add $2,$1
+    sub $3,1
+    add $5,$2
+    add $2,$1
+    mod $6,2
+    add $6,$0
+  lpe
+  add $7,$6
+lpe
+mov $0,$7
