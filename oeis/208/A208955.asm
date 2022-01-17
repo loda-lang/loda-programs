3dd7881914b44ef336218e06d0f951c6307b47f8
Christian Krause <me@ckrause.org>
2022-01-17 18:11:43 +0000

    added 84 programs

---
+++ oeis/208/A208955.asm
@@ -0,0 +1,8 @@
+; A208955: Expansion of phi(x) * phi(x^9) / chi(x^3)^2 in powers of x where phi(), chi() are Ramanujan theta functions.
+; Submitted by Christian Krause
+; 1,2,0,-2,-2,0,3,2,0,-2,-2,0,1,2,0,-2,-4,0,2,0,0,-4,-2,0,2,2,0,-2,-2,0,1,4,0,0,-2,0,4,2,0,-2,0,0,3,2,0,-2,-4,0,2,2,0,-4,0,0,0,4,0,-2,-2,0,2,2,0,0,-2,0,4,2,0,-2,-2,0,3,2,0,0,-4,0,2,2,0,-6,0,0,2,0,0,-2,-2,0,1,4,0,-2,-4,0,0,2,0,-2
+
+mov $2,-1
+pow $2,$0
+seq $0,121363 ; Expansion of q^(-1/4)(eta(q)*eta(q^6)*eta(q^9)/eta(q^3))^2/(eta(q^2)eta(q^18)) in powers of q.
+mul $0,$2
