935b9b55edbe1264e1484b60151d6c85b3edbdb0
Christian Krause <me@ckrause.org>
2022-01-20 17:52:19 +0000

    added 40 programs

---
+++ oeis/092/A092095.asm
@@ -0,0 +1,18 @@
+; A092095: a(n) = Sum_{i=0,1,2,..; n-k*i >= -n} |n-k*i| for k=4.
+; Submitted by Jamie Morken(w1)
+; 9,16,16,24,25,36,36,48,49,64,64,80,81,100,100
+
+lpb $0
+  sub $0,1
+  mov $3,$0
+  sub $3,$1
+  add $3,5
+  mov $1,$3
+  add $2,1
+  add $2,$3
+  mod $3,2
+  cmp $3,1
+  add $3,$2
+lpe
+mov $0,$3
+add $0,9
---
+++ oeis/139/A139327.asm
@@ -0,0 +1,27 @@
+; A139327: Write the first n^2 odd numbers consecutively in n rows of length n: a(n) = maximal number of primes in a row.
+; Submitted by Jamie Morken(w3)
+; 0,2,2,3,4,4,5,5,6,7,7,8,8,8,9,10,10,10,11,11,12,13,13,14,14,14,15,15,15,16,17,17,17,18,18,19,20,20,20,21,21,22,22,22,23,23,23,23,24,24,25,26,26,27,28,28,29,29,29,29,29,29,29,30,30,31,31,31,32,33,33,33,33,33
+
+add $0,1
+mul $0,2
+mov $3,1
+mov $4,3
+sub $4,$0
+lpb $0
+  sub $0,1
+  div $0,2
+  mul $0,2
+  trn $0,1
+  seq $0,151799 ; Version 2 of the "previous prime" function: largest prime < n.
+  sub $0,1
+  mov $5,$4
+  cmp $5,0
+  add $4,$5
+  div $2,$4
+  div $2,2
+  add $3,1
+  add $2,$3
+  add $4,$3
+lpe
+mov $0,$2
+sub $0,2
