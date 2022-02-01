be84ad9f9f02862da61af297380b569319cebab2
Simon Strandgaard <neoneye@gmail.com>
2022-02-01 20:52:47 +0100

    First version

---
+++ oeis/028/A028761.asm
@@ -0,0 +1,11 @@
+; A028761: Nonsquares mod 48.
+; Submitted by Simon Strandgaard
+; 2,3,5,6,7,8,10,11,12,13,14,15,17,18,19,20,21,22,23,24,26,27,28,29,30,31,32,34,35,37,38,39,40,41,42,43,44,45,46,47
+
+mov $2,$0
+lpb $2
+  sub $2,13
+  add $0,1
+  sub $2,14
+lpe
+seq $0,37 ; Numbers that are not squares (or, the nonsquares).
