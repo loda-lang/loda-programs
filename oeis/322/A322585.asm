2b5b21257cf83697e8a8e6c9992c585f4c3f5c83
Christian Krause <me@ckrause.org>
2022-02-03 20:09:17 +0000

    added 40 programs

---
+++ oeis/322/A322585.asm
@@ -0,0 +1,9 @@
+; A322585: a(n) = 1 if n is a product of primorial numbers (A002110), 0 otherwise.
+; Submitted by Jamie Morken(w2)
+; 1,1,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0
+
+seq $0,319627 ; Let f be the completely multiplicative function over the positive rational numbers defined by f(p) = A034386(p) for any prime number p; f constitutes a permutation of the positive rational numbers; let g be the inverse of f; for any n > 0, a(n) is the denominator of g(n).
+sub $0,1
+mov $2,$0
+cmp $2,0
+mov $0,$2
