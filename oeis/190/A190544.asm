7b455c7408912a53d0c9de1b3d905e6bbd98e00f
Christian Krause <me@ckrause.org>
2022-02-05 09:58:49 +0000

    added 39 programs

---
+++ oeis/190/A190544.asm
@@ -0,0 +1,12 @@
+; A190544: a(n) = [(bn+c)r]-b[nr]-[cr], where (r,b,c)=(sqrt(2),4,0) and []=floor.
+; Submitted by Jamie Morken(w3)
+; 1,3,0,2,0,1,3,1,2,0,2,3,1,3,0,2,0,1,3,1,2,0,2,3,1,3,0,2,0,1,3,1,2,0,1,3,1,2,0,2,3,1,3,0,2,0,1,3,1,2,0,2,3,1,3,0,2,0,1,3,1,2,0,2,3,1,3,0,2,3,1,3,0,2,0,1,3,1,2,0,2,3,1,3,0,2,0,1,3,1,2,0,2,3,1,3,0,2,0,1
+
+add $0,1
+mov $2,$0
+mul $2,3
+add $0,$2
+mul $0,2
+sub $0,1
+seq $0,70098 ; Number of integer triangles with perimeter n which are acute and isosceles.
+mod $0,4
