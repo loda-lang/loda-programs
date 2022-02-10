d6d38dcfc257ad8de6c969439b648348a018e41d
Christian Krause <me@ckrause.org>
2022-02-10 17:15:07 +0000

    added 77 programs

---
+++ oeis/044/A044104.asm
@@ -0,0 +1,25 @@
+; A044104: Numbers n such that string 0,2 occurs in the base 6 representation of n but not of n-1.
+; Submitted by Christian Krause
+; 38,74,110,146,182,218,228,254,290,326,362,398,434,444,470,506,542,578,614,650,660,686,722,758,794,830,866,876,902,938,974,1010,1046,1082,1092,1118,1154,1190,1226,1262,1298,1308,1334
+
+add $0,1
+lpb $0
+  mov $3,7
+  gcd $3,$0
+  sub $0,1
+  add $2,5
+  div $2,$3
+  mov $3,$2
+  pow $3,2
+  add $1,$3
+  mov $2,1
+lpe
+add $2,$1
+div $2,2
+mov $0,$2
+mul $0,2
+sub $0,8
+mul $0,2
+sub $0,36
+div $0,2
+add $0,38
