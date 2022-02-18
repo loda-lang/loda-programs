488fa640feeacb4ba372649519a50df93d2d8b97
Christian Krause <me@ckrause.org>
2022-02-18 16:49:34 +0000

    added 149 programs

---
+++ oeis/186/A186438.asm
@@ -0,0 +1,36 @@
+; A186438: Positive numbers whose squares end in two identical digits.
+; Submitted by Simon Strandgaard
+; 10,12,20,30,38,40,50,60,62,70,80,88,90,100,110,112,120,130,138,140,150,160,162,170,180,188,190,200,210,212,220,230,238,240,250,260,262,270,280,288,290,300,310,312,320,330,338,340,350,360,362,370,380,388,390,400,410,412
+
+mov $2,$0
+add $2,1
+lpb $2
+  mov $0,0
+  sub $2,1
+  sub $0,$2
+  mod $0,7
+  gcd $0,6
+  sub $0,2
+  lpb $0
+    div $0,11
+    mov $1,$0
+    add $0,1
+    seq $1,188298 ; Positions of 0 in A188297; complement of A188299.
+    sub $1,$0
+    mov $0,$1
+    add $0,1
+  lpe
+  mul $0,7
+  add $0,7
+  mov $1,$0
+  div $1,2
+  sub $0,$1
+  div $0,2
+  mod $1,2
+  add $1,$0
+  mov $0,$1
+  mul $0,2
+  add $0,2
+  add $3,$0
+lpe
+mov $0,$3
---
+++ oeis/292/A292342.asm
@@ -0,0 +1,7 @@
+; A292342: Number of singletons in the integer partition having viabin number n.
+; Submitted by Simon Strandgaard
+; 1,0,1,0,2,0,1,0,1,1,2,0,2,0,1,0,1,0,1,1,3,1,2,0,1,1,2,0,2,0,1,0,1,0,1,0,2,0,1,1,2,2,3,1,3,1,2,0,1,0,1,1,3,1,2,0,1,1,2,0,2,0,1,0,1,0,1,0,2,0,1,0,1,1,2,0,2,0,1,1,2,1,2,2,4,2,3,1,2,2,3,1
+
+mul $0,2
+add $0,1
+seq $0,290260 ; a(n) = number of isolated 0's in the binary representation of n.
