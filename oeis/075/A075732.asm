6d59f1913fd49015af8c012963a6fa0ccb2ea330
Christian Krause <me@ckrause.org>
2022-02-19 15:41:44 +0000

    added 48 programs

---
+++ oeis/075/A075732.asm
@@ -0,0 +1,6 @@
+; A075732: Fibonacci numbers F(k) for k not squarefree (A013929).
+; Submitted by Jamie Morken(w2)
+; 3,21,34,144,987,2584,6765,46368,75025,196418,317811,2178309,14930352,102334155,701408733,1134903170,4807526976,7778742049,12586269025,32951280099,86267571272,225851433717,1548008755920,6557470319842
+
+seq $0,13929 ; Numbers that are not squarefree. Numbers that are divisible by a square greater than 1. The complement of A005117.
+seq $0,45 ; Fibonacci numbers: F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
