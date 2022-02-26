3f8495c0dace303dfb33ec6719c176a5f0fed1f7
Christian Krause <me@ckrause.org>
2022-02-26 08:50:58 +0000

    updated programs

--- oeis/005/A005124.asm
+++ oeis/005/A005124.asm
@@ -1,17 +1,16 @@
 ; A005124: Numbers n such that 8n + 3 is prime.
-; Submitted by Christian Krause
+; Submitted by Jamie Morken(w4)
 ; 0,1,2,5,7,8,10,13,16,17,20,22,26,28,31,35,38,41,43,47,52,55,58,61,62,65,68,70,71,73,77,80,82,85,86,92,98,101,103,107,110,113,118,121,127,131,136,140,145,146,148,157,160,161,163,178,181,182,185,187,190,191,196,197,202,203,208,212,215,218,223,226,233,238,241,247,248,250,251,253,260,262,266,272,275,280,281,283,292,293,296,301,307,308,316,317,322,332,335,337
 
-mov $1,2
-mov $2,$0
-add $0,1
-pow $2,5
-lpb $2
-  mov $3,$1
-  seq $3,10051 ; Characteristic function of primes: 1 if n is prime, else 0.
-  sub $0,$3
-  add $1,8
-  sub $2,$0
+mov $2,2
+mov $3,$0
+pow $3,5
+lpb $3
+  seq $1,10051 ; Characteristic function of primes: 1 if n is prime, else 0.
+  sub $0,$1
+  add $2,8
+  mov $1,$2
+  sub $3,$0
 lpe
-mov $0,$1
+mov $0,$2
 div $0,8
--- oeis/010/A010051.asm
+++ oeis/010/A010051.asm
@@ -1,10 +1,24 @@
 ; A010051: Characteristic function of primes: 1 if n is prime, else 0.
-; Submitted by Jamie Morken(s4)
+; Submitted by Christian Krause
 ; 0,1,1,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0
 
-mov $1,$0
-mul $0,4
-seq $1,38548 ; Number of divisors of n that are at most sqrt(n).
-cmp $1,1
-trn $0,$1
-mod $0,2
+mov $5,$0
+add $0,1
+mov $2,$0
+lpb $0
+  add $4,1
+  min $0,$4
+  mov $3,$2
+  dif $3,$0
+  cmp $3,$2
+  sub $2,$4
+  mov $0,$2
+  cmp $3,0
+  add $1,$3
+  sub $2,$4
+lpe
+cmp $1,0
+mul $5,4
+trn $5,$1
+mod $5,2
+mov $0,$5
