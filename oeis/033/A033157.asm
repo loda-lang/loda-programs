0165f7528559d1e2bbb6c928f34d803818977ba7
Christian Krause <me@ckrause.org>
2022-03-01 19:14:36 +0000

    added 151 programs

---
+++ oeis/033/A033157.asm
@@ -0,0 +1,6 @@
+; A033157: Begins with (1, 4); avoids 3-term arithmetic progressions.
+; Submitted by Simon Strandgaard
+; 1,4,5,8,10,13,14,17,28,31,32,35,37,40,41,44,82,85,86,89,91,94,95,98,109,112,113,116,118,121,122,125,244,247,248,251,253,256,257,260,271,274,275,278,280,283,284,287,325,328,329,332,334,337,338,341,352,355,356,359,361
+
+seq $0,33161 ; Begins with (2, 5); avoids 3-term arithmetic progressions.
+sub $0,1
---
+++ oeis/177/A177121.asm
@@ -0,0 +1,8 @@
+; A177121: Square array T(n,k) read by antidiagonals up: T(n,k) = 1 if n=1; otherwise if n divides k then T(n,k) = -n+1; otherwise T(n,k) = 1.
+; Submitted by Jamie Morken(l1)
+; 1,1,1,1,-1,1,1,1,1,1,1,1,-2,-1,1,1,1,1,1,1,1,1,1,1,-3,1,-1,1,1,1,1,1,1,-2,1,1,1,1,1,1,-4,1,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-5,1,-3,-2,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-6,1,1,1,1,-1,1,1,1,1,1,1,1,1,1,1
+
+seq $0,191907 ; Square array read by antidiagonals up: T(n,k) = -(n-1) if n divides k, else 1.
+mov $2,$0
+cmp $2,0
+add $0,$2
