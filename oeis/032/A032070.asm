ec877ba0bd2ca4947b39af4c18a2f7262fd72861
Christian Krause <me@ckrause.org>
2022-03-02 17:14:39 +0000

    added 67 programs

---
+++ oeis/032/A032070.asm
@@ -0,0 +1,11 @@
+; A032070: Number of reversible strings with n labeled beads of 3 colors, no palindromes of more than 1 bead.
+; Submitted by Jamie Morken(w1)
+; 3,6,54,864,12960,252720,5307120,130636800,3527193600,106697606400,3521021011200,127105948569600,4957131994214400,208390202679859200,9377559120593664000,450260112212951040000
+
+mov $2,$0
+seq $0,32086 ; Number of reversible strings with n beads of 3 colors. If more than 1 bead, not palindromic.
+add $2,1
+lpb $2
+  mul $0,$2
+  sub $2,1
+lpe
---
+++ oeis/100/A100250.asm
@@ -0,0 +1,13 @@
+; A100250: Positions where values change in A100144.
+; Submitted by Jamie Morken(w1)
+; 1,2,4,8,14,24,38,56,78,106,140,180,226,278,336,404,482,570,668,776,894,1022,1160,1308,1466,1638,1824,2024,2238,2466,2708,2964,3234,3518,3816,4128,4454,4794,5148,5520,5910,6318,6744,7188,7650,8130,8628,9144,9678
+
+lpb $0
+  sub $0,1
+  mov $2,$0
+  max $2,0
+  seq $2,100143 ; Unique sequence with a(1)=1 where each a(n) occurs in the same order a(n) times consecutively in its sequence of first differences which contains no other terms.
+  add $1,$2
+lpe
+mov $0,$1
+add $0,1
