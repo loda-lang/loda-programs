edfd24fd88172118e95c9a0e21eb652c98afa5d2
Christian Krause <me@ckrause.org>
2022-03-03 16:11:41 +0000

    added 78 programs

---
+++ oeis/254/A254179.asm
@@ -0,0 +1,20 @@
+; A254179: Decimal expansion of atomic unit of time in s.
+; Submitted by Jamie Morken(w1)
+; 2,4,1,8,8,8,4,3,2,6
+
+add $0,1
+mul $0,9
+mov $1,2
+lpb $0
+  add $1,2
+  trn $0,$1
+  add $1,1
+  mov $2,$0
+  trn $0,$1
+  sub $1,1
+  add $1,$2
+  sub $1,$0
+lpe
+mov $0,$1
+add $0,3
+mod $0,10
---
+++ oeis/340/A340234.asm
@@ -0,0 +1,17 @@
+; A340234: Number of harmonious graphs with n edges and at most n vertices, allowing self-loops.
+; Submitted by Jamie Morken(w1)
+; 1,2,8,36,243,1728,16384,160000,1953125,24300000,362797056,5489031744,96889010407,1727094849536,35184372088832,722204136308736,16677181699666569,387420489000000000,10000000000000000000,259374246010000000000
+
+add $0,2
+lpb $0
+  max $0,$4
+  sub $0,1
+  pow $2,$1
+  add $1,1
+  mov $3,$4
+  pow $3,$0
+  mul $3,$2
+  mov $2,$0
+  mov $4,$1
+lpe
+mov $0,$3
