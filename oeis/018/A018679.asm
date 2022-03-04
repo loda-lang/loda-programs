c76a7670adc126c374bb35e2d0a536d577626b41
Christian Krause <me@ckrause.org>
2022-03-04 14:40:17 +0000

    added 91 programs

---
+++ oeis/018/A018679.asm
@@ -0,0 +1,38 @@
+; A018679: Divisors of 846.
+; Submitted by Jamie Morken(w1)
+; 1,2,3,6,9,18,47,94,141,282,423,846
+
+mov $1,1
+mov $2,1
+mov $3,1
+mov $8,$0
+mov $9,1
+lpb $8
+  mov $5,$2
+  lpb $5
+    add $2,1
+    mov $6,$2
+    mul $2,10
+    add $2,6
+    gcd $6,$3
+    cmp $6,1
+    cmp $6,0
+    sub $5,$6
+  lpe
+  add $2,1
+  mov $4,$3
+  mul $4,$2
+  mov $5,$0
+  mod $5,$4
+  sub $0,$5
+  div $5,$3
+  mov $3,$4
+  mov $7,$2
+  pow $7,$5
+  mul $1,$7
+  mov $7,$0
+  cmp $7,0
+  sub $8,$9
+  sub $9,$7
+lpe
+mov $0,$1
