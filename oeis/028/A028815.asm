461e487c29acbb30566f835a0a50214f217df75f
Christian Krause <me@ckrause.org>
2022-03-11 17:56:36 +0000

    updated 61 programs

--- oeis/028/A028815.asm
+++ oeis/028/A028815.asm
@@ -1,18 +1,10 @@
 ; A028815: a(n) = n-th prime + 1 (starting with 1).
-; Submitted by Simon Strandgaard
+; Submitted by Jamie Morken(w4)
 ; 2,3,4,6,8,12,14,18,20,24,30,32,38,42,44,48,54,60,62,68,72,74,80,84,90,98,102,104,108,110,114,128,132,138,140,150,152,158,164,168,174,180,182,192,194,198,200,212,224,228,230,234,240,242,252,258,264,270,272,278,282,284
 
-mov $2,2
-mov $3,$0
 sub $0,1
-pow $3,5
-lpb $3
-  mov $1,$2
-  seq $1,10051 ; Characteristic function of primes: 1 if n is prime, else 0.
-  sub $0,$1
-  add $2,2
-  sub $3,$0
-lpe
-pow $2,$0
-mov $0,$2
-add $0,2
+mov $2,1
+max $2,$0
+add $0,$2
+seq $0,173919 ; Numbers that are prime or one less than a prime.
+add $0,1
--- oeis/156/A156065.asm
+++ oeis/156/A156065.asm
@@ -6,7 +6,6 @@ mov $1,$0
 add $0,1
 lpb $0
   sub $0,1
-  cmp $2,5
   sub $2,2
   bin $2,$0
   trn $0,1
@@ -15,7 +14,7 @@ lpb $0
   bin $3,$1
   add $1,1
   mul $3,$2
-  mov $2,2
+  mov $2,0
   div $3,$1
   add $5,$3
 lpe
