0c66cbeefb79d80b97fc017aa6a951f01068cb90
Christian Krause <me@ckrause.org>
2022-03-15 17:55:41 +0000

    added 148 programs

---
+++ oeis/044/A044414.asm
@@ -0,0 +1,17 @@
+; A044414: Numbers n such that string 8,2 occurs in the base 10 representation of n but not of n-1.
+; Submitted by Simon Strandgaard
+; 82,182,282,382,482,582,682,782,820,882,982,1082,1182,1282,1382,1482,1582,1682,1782,1820,1882,1982,2082,2182,2282,2382,2482,2582,2682,2782,2820,2882,2982,3082,3182,3282,3382,3482,3582
+
+add $0,2
+seq $0,44337 ; Numbers n such that string 0,5 occurs in the base 10 representation of n but not of n-1.
+div $0,2
+sub $0,55
+mul $0,2
+mov $2,$0
+mod $0,4
+mul $0,2
+mov $1,$2
+add $1,$0
+add $1,$0
+mov $0,$1
+sub $0,120
---
+++ oeis/219/A219330.asm
@@ -0,0 +1,25 @@
+; A219330: Number of random selections (with replacement) needed from a normal population to assure a greater than one-half chance that the selected group contains the top 10th percentile individual, top 1st percentile individual, the 0.1 percentile, 0.01 percentile etc...
+; Submitted by Simon Strandgaard
+; 7,69,693,6932,69315,693147,6931472,69314718,693147181,6931471806,69314718056,693147180560,6931471805600,69314718055995,693147180559945,6931471805599453,69314718055994531,693147180559945310,6931471805599453094,69314718055994530942
+
+add $0,1
+mov $3,$0
+mul $3,6
+lpb $3
+  mul $1,$3
+  div $2,2
+  add $1,$2
+  div $1,$0
+  mul $2,$3
+  div $2,$0
+  add $2,4
+  add $2,$1
+  sub $3,1
+lpe
+mov $4,10
+pow $4,$0
+sub $4,2
+div $2,$4
+div $1,$2
+mov $0,$1
+add $0,2
