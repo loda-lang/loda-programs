1fb63fec360ff5a9324dc151c2ce6b5ce975e661
Christian Krause <me@ckrause.org>
2022-04-04 16:18:08 +0000

    updated 38 programs

--- oeis/100/A100672.asm
+++ oeis/100/A100672.asm
@@ -1,22 +1,7 @@
 ; A100672: Second least-significant bit in the binary expansion of the n-th prime.
-; Submitted by Jamie Morken(w1)
+; Submitted by Christian Krause
 ; 1,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,1,0,1,1,0,1,1,0,0,0,1,1,0,0,1,1,0,1,0,1,0,1,1,0,1,0,1,0,0,1,1,1,1,0,0,1,0,1,0,1,0,1,0,0,1,0,1,1,0,0,1,0,1,0,0,1,1,0,1,1,0,0,0,0,1,0,1,0,1,1,0,0,0,1,1,1,1,1,1,1,0,0,1,0
 
-sub $0,2
-mov $2,6
-mov $3,$0
-pow $3,5
-lpb $3
-  mov $1,$2
-  seq $1,10051 ; Characteristic function of primes: 1 if n is prime, else 0.
-  sub $0,$1
-  add $2,2
-  sub $3,$0
-lpe
-pow $2,$0
-mov $0,$2
-mul $0,2
-sub $0,5
-div $0,4
-add $0,2
+trn $0,1
+seq $0,274332 ; Team size n for which there exists a balanced tournament for 2n+1 players so that in 2n+1 matches each player plays exactly n-1 times with and n times against each other player.
 mod $0,2
