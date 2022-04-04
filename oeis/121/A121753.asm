3bf60d5da96c12114ff0e9fc69cdd134ff7f7328
Christian Krause <me@ckrause.org>
2022-04-04 16:14:37 +0000

    added 23 programs

---
+++ oeis/121/A121753.asm
@@ -0,0 +1,17 @@
+; A121753: Number of deco polyominoes of height n in which all columns end at an odd level. A deco polyomino is a directed column-convex polyomino in which the height, measured along the diagonal, is attained only in the last column.
+; Submitted by Jamie Morken(w1)
+; 1,1,2,6,16,62,230,1114,5268,30702,176226,1201638,8107464,63339702,491010102,4324845834,37867131900,371275954758,3623124865986,39137296073094,421150512316032,4969568447400366,58455531552960198
+
+mov $2,1
+lpb $0
+  sub $0,1
+  mov $4,$3
+  mul $3,$1
+  mov $1,$0
+  div $1,2
+  add $3,$2
+  add $4,$2
+  mul $2,$1
+  add $2,$4
+lpe
+mov $0,$2
