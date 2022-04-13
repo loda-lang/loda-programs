7d136dcf9de39c547b0bee7945dd1fe73d1d12ad
Simon Strandgaard <neoneye@gmail.com>
2022-04-13 11:07:10 +0200

    Updated programs

--- oeis/315/A315418.asm
+++ oeis/315/A315418.asm
@@ -1,24 +1,12 @@
 ; A315418: Coordination sequence Gal.3.21.3 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
+; Submitted by Simon Strandgaard
 ; 1,6,11,15,20,26,32,37,41,46,52,58,63,67,72,78,84,89,93,98,104,110,115,119,124,130,136,141,145,150,156,162,167,171,176,182,188,193,197,202,208,214,219,223,228,234,240,245,249,254
 
 mov $1,$0
-mul $0,2
-add $0,6
-mov $3,3
-mov $4,3
-lpb $0
-  mov $2,$0
-  sub $2,4
-  add $3,6
-  add $4,6
-  trn $2,$4
-  add $2,$3
-  mov $0,$2
-lpe
-sub $0,6
-trn $0,1
-lpb $1
-  sub $1,1
-  add $0,4
-lpe
-add $0,1
+seq $0,265227 ; Nonnegative m for which k*floor(m^2/9) = floor(k*m^2/9), with 2 < k < 9.
+mov $2,$0
+dif $0,-1
+cmp $2,0
+mul $1,7
+add $0,$2
+add $0,$1
