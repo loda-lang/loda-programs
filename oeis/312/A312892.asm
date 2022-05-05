42e3d9464e4b8435d8e78b5ac53613a6fd15017b
Simon Strandgaard <neoneye@gmail.com>
2022-05-06 00:00:43 +0200

    Updated programs

--- oeis/312/A312892.asm
+++ oeis/312/A312892.asm
@@ -3,7 +3,7 @@
 ; 1,4,9,13,17,21,25,29,33,38,42,46,51,55,59,63,67,71,75,80,84,88,93,97,101,105,109,113,117,122,126,130,135,139,143,147,151,155,159,164,168,172,177,181,185,189,193,197,201,206
 
 mov $1,$0
-seq $1,315547
+seq $1,315547 ; Coordination sequence Gal.5.295.2 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
 mov $2,$0
 mul $2,3
 mul $0,7
