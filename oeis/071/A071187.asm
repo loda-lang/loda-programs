6413788468622d0152d4f586119adccaadb71d90
Christian Krause <me@ckrause.org>
2022-05-08 07:02:00 +0000

    deleted 10 programs

--- oeis/071/A071187.asm
+++
@@ -1,5 +0,0 @@
-; A071187: Smallest prime factor of number of divisors of n.
-; 1,2,2,3,2,2,2,2,3,2,2,2,2,2,2,5,2,2,2,2,2,2,2,2,3,2,2,2,2,2,2,2,2,2,2,3,2,2,2,2,2,2,2,2,2,2,2,2,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,7,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3
-
-seq $0,52409 ; a(n) = largest integer power m for which a representation of the form n = k^m exists (for some k).
-seq $0,20639 ; Lpf(n): least prime dividing n (when n > 1); a(1) = 1. Or, smallest prime factor of n, or smallest prime divisor of n.
