1fd1119b779f3608a30860725829b637a9e82575
Simon Strandgaard <neoneye@gmail.com>
2022-05-10 18:32:18 +0200

    Updated programs

--- oeis/327/A327916.asm
+++ oeis/327/A327916.asm
@@ -5,15 +5,12 @@
 lpb $0
   add $1,1
   sub $0,$1
-  mov $2,$1
 lpe
-add $1,$0
-mul $1,2
-add $2,1
+mov $2,$1
 sub $2,$0
+add $1,$0
+add $1,1
 mov $0,2
 pow $0,$2
-add $1,2
 mul $1,$0
 mov $0,$1
-div $0,4
