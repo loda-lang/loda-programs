6afa4948af64cb87ce83485cae3946a14992a20d
Christian Krause <me@ckrause.org>
2022-05-17 16:00:23 +0000

    deleted 6 programs

--- oeis/174/A174898.asm
+++
@@ -1,7 +0,0 @@
-; A174898: a(n) = characteristic function of numbers k such that A007955(m) = k has no solution for any m, where A007955(m) = product of divisors of m.
-; Submitted by fzs600
-; 0,0,0,1,0,1,0,0,1,1,0,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,0,1,1,1,1,0,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,0,1,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,0
-
-seq $0,174897 ; a(n) = characteristic function of numbers k such that A007955(m) = k has solution for some m, where A007955(m) = product of divisors of m.
-mul $0,-1
-add $0,1
