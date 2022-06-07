88172c3f56e8124a8bd00ed71167a1cfa8aba6d2
Christian Krause <me@ckrause.org>
2022-06-07 16:37:37 +0000

    deleted 5 programs

--- oeis/284/A284391.asm
+++
@@ -1,8 +0,0 @@
-; A284391: 1-limiting word of the morphism 0 -> 1, 1 -> 001.
-; Submitted by Christian Krause
-; 1,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,0,0,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,0,0,1,0,0
-
-trn $0,1
-seq $0,194688 ; First differences of A036554 (numbers whose binary representation ends in an odd number of zeros).
-div $0,2
-sub $0,1
--- oeis/326/A326845.asm
+++
@@ -1,7 +0,0 @@
-; A326845: Sum times maximum of the integer partition with Heinz number n.
-; Submitted by Soulfly
-; 0,1,4,2,9,6,16,3,8,12,25,8,36,20,15,4,49,10,64,15,24,30,81,10,18,42,12,24,100,18,121,5,35,56,28,12,144,72,48,18,169,28,196,35,21,90,225,12,32,21,63,48,256,14,40,28,80,110,289,21,324,132,32,6,54,40
-
-seq $0,108951 ; Primorial inflation of n: Fully multiplicative with a(p) = p# for prime p, where x# is the primorial A034386(x).
-sub $0,1
-seq $0,113901 ; Product of omega(n) and bigomega(n) = A001221(n)*A001222(n), where omega(x): number of distinct prime divisors of x. bigomega(x): number of prime divisors of x, counted with multiplicity.
