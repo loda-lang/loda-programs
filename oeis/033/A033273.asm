484ebfc4eb9228b6c55d5338a6e4ebd8d5c2865e
Christian Krause <me@ckrause.org>
2022-06-14 19:26:53 +0000

    deleted 31 programs

--- oeis/033/A033273.asm
+++
@@ -1,9 +0,0 @@
-; A033273: Number of nonprime divisors of n.
-; Submitted by Christian Krause
-; 1,1,1,2,1,2,1,3,2,2,1,4,1,2,2,4,1,4,1,4,2,2,1,6,2,2,3,4,1,5,1,5,2,2,2,7,1,2,2,6,1,5,1,4,4,2,1,8,2,4,2,4,1,6,2,6,2,2,1,9,1,2,4,6,2,5,1,4,2,5,1,10,1,2,4,4,2,5,1,8,4,2,1,9,2,2,2,6,1,9,2,4,2,2,2,10,1,4,4,7
-
-mov $1,$0
-seq $0,322307 ; Number of multisets in the swell of the n-th multiset multisystem.
-seq $1,5 ; d(n) (also called tau(n) or sigma_0(n)), the number of divisors of n.
-sub $1,$0
-mov $0,$1
--- oeis/241/A241422.asm
+++
@@ -1,6 +0,0 @@
-; A241422: Limit-reverse of the infinite Fibonacci word A003849 with first term as initial block.
-; 0,1,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,0,0,1,0,1,0,0
-
-add $0,100
-seq $0,14675 ; The infinite Fibonacci word (start with 1, apply 1->2, 2->21, take limit).
-mod $0,2
