9502d691c781d72cf2f52ab3f6da4dfa4e8bfea1
Christian Krause <me@ckrause.org>
2022-06-19 18:11:49 +0000

    deleted 9 programs

--- oeis/053/A053025.asm
+++
@@ -1,9 +0,0 @@
-; A053025: Number of iterations of A000005 required to reach 2 when started at n!.
-; Submitted by jmorken
-; 1,1,4,5,4,6,7,7,7,5,7,7,7,7,7,6,7,7,7,7,7,8,7,7,7,7,6,6,8,6,7,6,7,8,6,8,7,5,7,6,8,6,6,8,8,8,8,8,8,6,8,8,8,8,8,8,8,8,8,8,7,8,7,8,7,8,8,8,8,8,8,7,8,8,8,8,8,8,7,8,8,8,8,8,8,8,8,8,8,8,7,8,7,8,8,8,7,8,8,7
-
-mul $0,2
-seq $0,249769 ; Sequence of distinct least positive numbers such that the average of the first n terms is a factorial.
-sub $0,1
-seq $0,36459 ; Number of iterations required to reach stationary value when repeatedly applying d, the number of divisors function (A000005).
-add $0,1
--- oeis/351/A351283.asm
+++
@@ -1,5 +0,0 @@
-; A351283: a(n) is the number of solutions of the equation 2*(x - 1)*y - (x - 3)*x = 2*n for 0 < x <= y.
-; Submitted by Christian Krause
-; 0,1,1,1,2,1,2,1,3,1,2,2,2,2,3,1,2,3,2,2,3,2,2,2,3,2,4,1,2,4,2,1,4,2,4,2,2,2,4,2,2,4,2,2,5,2,2,2,3,3,4,2,2,4,3,2,4,2,2,4,2,2,6,1,4,3,2,2,4,4,2,3,2,2,6,2,4,3,2,2,5,2,2,4,4,2,4,2,2
-
-seq $0,136107 ; Number of representations of n as the difference of two positive triangular numbers.
