9365c3cf0f2206edaea73fa661c7bae1e939461e
Christian Krause <me@ckrause.org>
2022-06-24 17:50:28 +0000

    deleted 16 programs

--- oeis/063/A063845.asm
+++
@@ -1,6 +0,0 @@
-; A063845: a(n) = sigma(usigma(n)), where usigma(n) is the sum of unitary divisors of n (A034448) and sigma(n) is the sum of the divisors (A000203).
-; Submitted by Jon Maiga
-; 1,4,7,6,12,28,15,13,18,39,28,42,24,60,60,18,39,72,42,72,63,91,60,91,42,96,56,90,72,195,63,48,124,120,124,93,60,168,120,120,96,252,84,168,168,195,124,126,93,168,195,144,120,224,195,195,186,234,168,360,96,252,186,84,224,403,126,234,252,403,195,234,114,240,210,217,252,480,186,216,126,312,224,378,280,336,360,280,234,546,248,360,255,403,360,336,171,372,360,252
-
-seq $0,345451 ; Sum of the unitary divisors of n whose square does not divide n.
-seq $0,203 ; a(n) = sigma(n), the sum of the divisors of n. Also called sigma_1(n).
--- oeis/080/A080405.asm
+++
@@ -1,8 +0,0 @@
-; A080405: Number of distinct primes dividing n-th Catalan number.
-; Submitted by http://kodeks.karelia.ru/
-; 0,0,1,1,2,3,3,3,4,4,4,5,5,5,6,6,7,8,8,9,9,9,9,9,9,9,9,10,11,11,11,11,12,13,13,13,13,13,13,14,14,15,15,15,16,16,16,17,17,17,17,17,17,19,18,18,18,18,19,19,20,20,20,20,21,22,22,23,22,22,22,23,23,23,24,24,25,25,25,26,26,26,26,26,26,26,27,28,27,27,28,28,28,28,28,29,29,29,29,30
-
-seq $0,108 ; Catalan numbers: C(n) = binomial(2n,n)/(n+1) = (2n)!/(n!(n+1)!).
-sub $0,1
-seq $0,83399 ; Number of divisors of n that are not divisors of other divisors of n.
-sub $0,1
