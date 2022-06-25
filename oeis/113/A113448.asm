372f1b02d0614637f4889c094c662f8863a39735
Christian Krause <me@ckrause.org>
2022-06-25 18:11:01 +0000

    deleted 16 programs

--- oeis/113/A113448.asm
+++
@@ -1,6 +0,0 @@
-; A113448: Expansion of (eta(q^2)^2 * eta(q^9) * eta(q^18)) / (eta(q) * eta(q^6)) in powers of q.
-; Submitted by Jamie Morken(s4)
-; 1,1,0,1,0,0,2,1,0,0,0,0,2,2,0,1,0,0,2,0,0,0,0,0,1,2,0,2,0,0,2,1,0,0,0,0,2,2,0,0,0,0,2,0,0,0,0,0,3,1,0,2,0,0,0,2,0,0,0,0,2,2,0,1,0,0,2,0,0,0,0,0,2,2,0,2,0,0,2,0,0,0,0,0,0,2,0,0,0,0,4,0,0,0,0,0,2,3,0,1
-
-seq $0,265 ; Remove all factors of 2 from n; or largest odd divisor of n; or odd part of n.
-seq $0,45833 ; Expansion of eta(q^9)^3 / eta(q^3) in powers of q.
