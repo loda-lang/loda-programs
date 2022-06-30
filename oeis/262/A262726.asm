5cf0131b99b7218b4faf188f1a35c4d7b23d1cf6
Christian Krause <me@ckrause.org>
2022-06-30 20:07:18 +0000

    deleted 22 programs

--- oeis/262/A262726.asm
+++
@@ -1,7 +0,0 @@
-; A262726: Expansion of phi(-x) * psi(x^6) in powers of x where phi(), psi() are Ramanujan theta functions.
-; Submitted by Christian Krause
-; 1,-2,0,0,2,0,1,-2,0,-2,2,0,0,0,0,-2,2,0,1,-2,0,0,4,0,0,-2,0,-2,0,0,0,-2,0,0,2,0,3,-2,0,0,2,0,2,-2,0,-2,0,0,0,-2,0,0,2,0,2,-2,0,0,0,0,1,-4,0,0,4,0,0,-2,0,-2,2,0,2,0,0,0,2,0,0,0,0,-2,2,0,0,-4,0,-2,0,0,1,-2,0,0,2,0,2,0,0,-4
-
-mul $0,3
-add $0,2
-seq $0,262774 ; Expansion of psi(x^2) * phi(-x^3) in powers of x where phi(), psi() are Ramanujan theta functions.
