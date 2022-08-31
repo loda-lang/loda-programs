9b503d2a92d0091c91c785787a9f7054f6e43725
Christian Krause <me@ckrause.org>
2022-08-31 16:46:13 +0000

    deleted 41 programs

--- oeis/182/A182936.asm
+++
@@ -1,9 +0,0 @@
-; A182936: Greatest common divisor of the proper divisors of n, 0 if there are none.
-; 0,0,0,2,0,1,0,2,3,1,0,1,0,1,1,2,0,1,0,1,1,1,0,1,5,1,3,1,0,1,0,2,1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,7,1,1,1,0,1,1,1,1,1,0,1,0,1,1,2,1,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,3,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1
-
-lpb $0
-  seq $0,14963 ; Exponential of Mangoldt function M(n): a(n) = 1 unless n is a prime or prime power when a(n) = that prime.
-  mov $1,$0
-  sub $0,1
-lpe
-mov $0,$1
