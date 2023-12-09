1e2a00444fefcb0be72113f4d1db7ad26e0ea8a6
Christian Krause <me@ckrause.org>
2023-12-09 10:25:40 +0000

    simplify sums and products in formulas

--- oeis/193/A193649.asm
+++ oeis/193/A193649.asm
@@ -1,7 +1,7 @@
 ; A193649: Q-residue of the (n+1)st Fibonacci polynomial, where Q is the triangular array (t(i,j)) given by t(i,j)=1.  (See Comments.)
 ; Submitted by Science United
 ; 1,1,3,5,15,33,91,221,583,1465,3795,9653,24831,63441,162763,416525,1067575,2733673,7003971,17938661,45954543,117709185,301527355,772364093,1978473511
-; Formula: a(n) = b(n)/4+1, b(n) = b(n-1)+c(n-1), b(1) = 2, b(0) = 2, c(n) = 4*b(n-1)+4*c(n-1)-4*c(n-1), c(1) = 8, c(0) = 0
+; Formula: a(n) = b(n)/4+1, b(n) = 4*b(n-2)+b(n-1), b(1) = 2, b(0) = 2
 
 mov $1,2
 lpb $0
--- oeis/206/A206549.asm
+++ oeis/206/A206549.asm
@@ -1,7 +1,7 @@
 ; A206549: Nontrivial solutions of x^2 == 1 (Modd p), p an odd prime, for odd restricted residue classes Modd p.
 ; Submitted by Stony666
 ; 3,5,13,17,31,9,23,11,27,55,75,91,33,15,37,105,129,93,19,81,183,107,89,177,241,187,217,53,155,25,203,189,213,311,269,115,63,381,143,29,179,67,109,413,301,235,489,439,483,553
-; Formula: a(n) = A070669(4*A002144(n)-2*A002144(n)-1)
+; Formula: a(n) = A070669(2*A002144(n)-1)
 
 seq $0,2144 ; Pythagorean primes: primes of form 4*k + 1.
 mul $0,2
