545b75d496539b90b915ecd1d209e97d4fac5c44
Christian Krause <me@ckrause.org>
2023-12-09 10:39:27 +0000

    simplify sums and products in formulas

--- oeis/353/A353446.asm
+++ oeis/353/A353446.asm
@@ -1,7 +1,7 @@
 ; A353446: Let g be the inverse Möbius transform of the Eisenstein integer-valued function f defined in A353445. a(n) is twice the real part of g(n).
 ; Submitted by ChelseaOilman
 ; 2,1,1,0,1,2,1,2,0,-1,1,0,1,2,2,1,1,0,1,0,-1,-1,1,1,0,2,2,0,1,1,1,0,2,-1,2,0,1,2,-1,1,1,1,1,0,0,-1,1,2,0,0,2,0,1,1,-1,1,-1,2,1,0,1,-1,0,2,2,1,1,0,2,1,1,0,1,2,0,0,2,1,1,-1
-; Formula: a(n) = 2*A000005(n)-A353351(n)-2*A353351(n)
+; Formula: a(n) = 2*A000005(n)-3*A353351(n)
 
 mov $1,$0
 seq $0,353351 ; Number of divisors d of n for which A048675(d) is not a multiple of 3.
