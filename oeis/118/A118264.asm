0960f23ec244b2c57f11545308d89425639abbe8
Christian Krause <me@ckrause.org>
2023-12-10 13:50:38 +0000

    updated 368 programs

--- oeis/118/A118264.asm
+++ oeis/118/A118264.asm
@@ -1,7 +1,7 @@
 ; A118264: Coefficient of q^n in (1-q)^3/(1-3q); dimensions of the enveloping algebra of the derived free Lie algebra on 3 letters.
 ; Submitted by [TA]crashtech
 ; 1,0,3,8,24,72,216,648,1944,5832,17496,52488,157464,472392,1417176,4251528,12754584,38263752,114791256,344373768,1033121304,3099363912,9298091736,27894275208,83682825624,251048476872,753145430616
-; Formula: a(n) = b(n+1), b(n) = 3*b(n-1)+binomial(n-5,n-1), b(2) = 0, b(1) = 1, b(0) = 0
+; Formula: a(n) = b(n+1), b(n) = 3*b(n-1)+binomial(c(n-1)-4,n-1), b(2) = 0, b(1) = 1, b(0) = 0, c(n) = c(n-1)+1, c(2) = 2, c(1) = 1, c(0) = 0
 
 add $0,1
 lpb $0
