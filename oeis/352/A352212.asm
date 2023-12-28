2157d0a7fc395470a10e2edd73c0d225f2f8d4a0
Christian Krause <me@ckrause.org>
2023-12-28 12:53:38 +0000

    updated 103 programs

--- oeis/352/A352212.asm
+++ oeis/352/A352212.asm
@@ -1,7 +1,7 @@
 ; A352212: Largest number of maximal triangle-free node-induced subgraphs of an n-node graph.
 ; Submitted by USTL-FIL (Lille Fr)
 ; 1,1,3,6,10,15,21,36,60
-; Formula: a(n) = (c(n)+d(n))/2+1, b(n) = max(d(n-2)+d(n-3),b(n-1))+2, b(5) = 10, b(4) = 8, b(3) = 6, b(2) = 4, b(1) = 2, b(0) = 0, c(n) = b(n-1), c(4) = 6, c(3) = 4, c(2) = 2, c(1) = 0, c(0) = 0, d(n) = b(n-1)+d(n-1), d(4) = 12, d(3) = 6, d(2) = 2, d(1) = 0, d(0) = 0
+; Formula: a(n) = (d(n)+e(n))/2+1, b(n) = max(c(n-2)+c(n-3),b(n-1))+2, b(8) = 52, b(7) = 34, b(6) = 20, b(5) = 10, b(4) = 8, b(3) = 6, b(2) = 4, b(1) = 2, b(0) = 0, c(n) = b(n-1)+c(n-1), c(6) = 30, c(5) = 20, c(4) = 12, c(3) = 6, c(2) = 2, c(1) = 0, c(0) = 0, d(n) = b(n-1), d(4) = 6, d(3) = 4, d(2) = 2, d(1) = 0, d(0) = 0, e(n) = b(n-1)+e(n-1), e(4) = 12, e(3) = 6, e(2) = 2, e(1) = 0, e(0) = 0
 
 lpb $0
   sub $0,1
--- oeis/352/A352737.asm
+++ oeis/352/A352737.asm
@@ -1,7 +1,7 @@
 ; A352737: Number of oriented two-component rational links (or two-bridge links) with crossing number n (a chiral pair is counted as two distinct ones).
 ; Submitted by Simon Strandgaard
 ; 2,0,4,2,10,10,30,42,102,170,374,682,1430,2730,5590,10922,22102,43690,87894,174762,350550,699050,1400150,2796202,5596502,11184810,22377814,44739242,89494870,178956970,357946710,715827882,1431721302,2863311530,5726754134,11453246122
-; Formula: a(n) = 2*c(n-2)+2*a(n-2), a(3) = 2, a(2) = 4, a(1) = 0, a(0) = 2, b(n) = 2*b(n-2)+b(n-1), b(3) = 5, b(2) = 3, b(1) = 1, b(0) = 1, c(n) = b(n-1), c(3) = 3, c(2) = 1, c(1) = 1, c(0) = 0
+; Formula: a(n) = 2*b(n-2)+2*a(n-2), a(3) = 2, a(2) = 4, a(1) = 0, a(0) = 2, b(n) = 2*b(n-2)+b(n-1), b(4) = 5, b(3) = 3, b(2) = 1, b(1) = 1, b(0) = 0
 
 mov $1,1
 mov $4,2
