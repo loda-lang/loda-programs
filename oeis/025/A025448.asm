4c1305d13a3ba38ba60ca2abdc1db6734843582e
loader3229 <1010903229@qq.com>
2025-10-16 02:02:13 +0800

    Deleted some incorrect programs found in partial check (#407)

--- oeis/025/A025448.asm
+++
@@ -1,8 +0,0 @@
-; A025448: Number of partitions of n into 4 nonnegative cubes.
-; Submitted by rajab
-; 1,1,1,1,1,0,0,0,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1,1,0,1,1,1,1,0,1,0,0,1,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,1,1,1,0,0,0,0,0,1,1,1,1,1,1,0,0,1,0,1,1,1,0,0,0,0,0
-; Formula: a(n) = A001221(A173678(n)+1)
-
-seq $0,173678 ; Number of ways of writing n as a sum of 4 nonnegative cubes.
-add $0,1
-seq $0,1221 ; Number of distinct primes dividing n (also called omega(n)).
--- oeis/229/A229047.asm
+++
@@ -1,7 +0,0 @@
-; A229047: Replace all '11' => '101' in the binary representation of n, treat the result as representation of a(n) in base of Fibonacci numbers (A014417).
-; Submitted by Aurum
-; 0,1,2,4,3,4,7,12,5,6,7,12,11,12,20,33,8,9,10,17,11,12,20,33,18,19,20,33,32,33,54,88,13,14,15,25,16,17,28,46,18,19,20,33,32,33,54,88,29,30,31,51,32,33,54,88,52,53,54,88,87,88,143,232,21,22,23,38,24,25,41
-; Formula: a(n) = A022290(A163809(n))
-
-seq $0,163809 ; Write n in binary. Insert a 0 in the middle of each pair of two consecutive 1's. a(n) = the decimal value of the result.
-seq $0,22290 ; Replace 2^k in binary expansion of n with Fibonacci(k+2).
