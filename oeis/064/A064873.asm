db96eb2065bbab399eb10db54b20d9ea0aea3128
loader3229 <1010903229@qq.com>
2025-12-05 16:07:55 +0800

    commit

--- oeis/064/A064873.asm
+++
@@ -1,7 +0,0 @@
-; A064873: First of four sequences representing the lexicographical minimal decomposition of n in 4 squares: n = a(n)^2 + A064874(n)^2 + A064875(n)^2 + A064876(n)^2.
-; Submitted by loader3229
-; 0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1
-
-dir $0,4
-mod $0,8
-equ $0,7
--- oeis/180/A180006.asm
+++
@@ -1,9 +0,0 @@
-; A180006: Composite numbers that can be obtained from primes by interchanging the first and last digits.
-; Submitted by [AF>Libristes] Dudumomo
-; 91,32,92,14,34,74,35,95,16,76,38,98,301,901,721,731,931,361,371,391,791,112,322,722,922,332,932,142,152,752,362,962,172,772,182,382,392,703,713,133,943,763,973,793,104,904,914,124,134,334,934,344,944,754
-; Formula: a(n) = A004086(A076056(n))
-
-#offset 1
-
-seq $0,76056 ; Primes which when read backwards are composite numbers.
-seq $0,4086 ; Read n backwards (referred to as R(n) in many sequences).
